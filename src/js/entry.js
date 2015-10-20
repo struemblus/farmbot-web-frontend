db0ce41b4c01a84200f63572e1ebf9ed610cc2ef
Rick Carlino <rick.carlino@gmail.com>
2015-10-20 09:52:11 -0500

    add font icons

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,4 +1,23 @@
-// require("./farm_designer");
 require("./routes")
 require("!style!css!../css/bootstrap.css");
 require("!style!css!sass!../css/farmbot.scss");
+
+function loadjscssfile(filename, filetype){
+    if (filetype=="js"){ //if filename is a external JavaScript file
+        var fileref=document.createElement('script')
+        fileref.setAttribute("type","text/javascript")
+        fileref.setAttribute("src", filename)
+    }
+    else if (filetype=="css"){ //if filename is an external CSS file
+        var fileref=document.createElement("link")
+        fileref.setAttribute("rel", "stylesheet")
+        fileref.setAttribute("type", "text/css")
+        fileref.setAttribute("href", filename)
+    }
+    if (typeof fileref!="undefined")
+        document.getElementsByTagName("head")[0].appendChild(fileref)
+}
+
+loadjscssfile(
+  "//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css",
+  "css");
