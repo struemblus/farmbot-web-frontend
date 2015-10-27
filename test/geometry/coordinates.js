7d44e4034b3e1d2c0dd1f608984ff82207514dd1
Rick Carlino <rick.carlino@gmail.com>
2015-10-27 07:55:19 -0500

    Add specs for coordinate translation stuff

---
+++ test/geometry/coordinates.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React from 'react'
+import TestUtils from 'react/lib/ReactTestUtils'
+import fromScreenToGarden from '../../src/js/geometry/coordinates.js'
+
+describe('fromScreenToGarden', function() {
+  it('converts screen coordinates to garden coordinates', function() {
+    console.log("Hello, world!");
+    expect(true).toBe(true);
+  });
+});
