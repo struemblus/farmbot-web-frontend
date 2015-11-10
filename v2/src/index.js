a8ed8edc4c94551b28ef61883eb41e47f55971bb
Rick Carlino <rick.carlino@gmail.com>
2015-11-10 15:29:43 -0500

    Added CSS to new scaffold

--- v2/src/index.js
+++ v2/src/index.js
@@ -4,8 +4,18 @@ import { Provider } from 'react-redux';
 import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import { ReduxRouter } from 'redux-router';
 import configureStore from './configureStore';
+import { loadFromCdn } from './load_from_cdn';
 
 const store = configureStore();
+
+require("!style!css!sass!./css/farmbot.scss");
+loadFromCdn(
+  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css",
+  "css");
+loadFromCdn(
+  "//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css",
+  "css");
+
 
 class Root extends Component {
   render() {
