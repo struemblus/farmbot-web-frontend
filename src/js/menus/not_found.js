c7ec219e872919e2c78e5846e9364d3014a4909a
Rick Carlino <rick.carlino@gmail.com>
2015-11-23 15:26:48 -0500

    [STABLE] Replace old app tooling with v2 scaffold that @robertlong made

--- src/js/menus/not_found.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export var NotFound = React.createClass({
-  render: function() {
-    return <p> Cant find that page. :( <a href="#s/designer">Back to safety</a></p>;
-  }
-});
--- src/js/redux/store.js
+++
@@ -1,9 +0,0 @@
-// A pretty basic Redux store, with default state provided by the 'appState'
-// module (will look in localStorage for previous state when bootstrapping).
-import { createStore, applyMiddleware, compose } from 'redux';
-import { appState } from './app_state';
-import { reducer } from './reducer';
-
-var store = createStore(reducer, appState.getState());
-
-export { store };
