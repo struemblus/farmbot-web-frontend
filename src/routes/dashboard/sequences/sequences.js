fdd74bec58f37dab1c754821f165eb423f6ed3b0
Rory Aronson <rory@roryaronson.com>
2015-12-11 21:04:37 -0800

    put sequence editor widget in its own file

--- src/routes/dashboard/sequences/sequences.js
+++ src/routes/dashboard/sequences/sequences.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navbar } from '../../../components/navbar';
 import { OperationsWidget } from '../../../routes/dashboard/sequences/operations_widget';
 import { SequencesWidget } from '../../../routes/dashboard/sequences/sequences_widget';
+import { SequenceEditorWidget } from '../../../routes/dashboard/sequences/sequence_editor_widget';
 
 export var Sequences = React.createClass({
   render: function() {
@@ -11,59 +12,12 @@ export var Sequences = React.createClass({
         <div className="all-content-wrapper">
           <div>
             <div className="row">
-
               <div className="col-md-3 col-sm-12">
                 <OperationsWidget/>
               </div>
 
               <div className="col-md-6 col-sm-12">
-                <div>
-                  <div className="widget-wrapper">
-                    <div className="row">
-                      <div className="col-sm-12">
-                        <button className="green button-like widget-control">
-                          Save
-                        </button>
-                        <button className="yellow button-like widget-control">
-                          Execute
-                        </button>
-                        <button className="red button-like widget-control">
-                          Delete
-                        </button>
-                        <div className="widget-header">
-                          <h5>Sequence Editor</h5>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-sm-12">
-                        <div className="widget-content">
-                          <div className="row">
-                            <div className="col-sm-12">
-                              <input id="right-label" placeholder="Sequence Name" type="text" />
-                            </div>
-                          </div>
-                          <div className="row">
-                            <div className="col-sm-12">
-                              <label>Sequence Parameters:</label>
-                              <a className="tiny expand button round dark-gray" href="#">PLANT-ID/PLANT-GROUP-ID</a>
-                            </div>
-                          </div>
-                          <div className="row">
-                            <div as-sortable="dragControlListeners" className="col-sm-12">
-                              {/* ngRepeat: step in sequence.steps | orderBy: 'position' */}
-                            </div>
-                          </div>
-                          <div className="row">
-                            <div className="col-sm-12">
-                              <div className="drag-drop-area padding">DROP OPERATIONS AND SEQUENCES HERE</div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <SequenceEditorWidget/>
               </div>
 
               <div className="col-md-3 col-sm-12">
