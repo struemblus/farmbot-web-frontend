906276f1a085f28452c44b7f88704c77c308694a
Rick Carlino <rick.carlino@gmail.com>
2016-04-04 00:11:41 -0500

    Cruft removal

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export var CONFIG = {
-  // The REST API endpoint that stores user auth, device settings, etc.
-  FARMBOT_API_URL: "//staging.farmbot.io",
-  // The Websocket gateway for IoT communication.
-  MESHBLU_URL: "wss://meshblu.octoblu.com/ws/v2",
-  // The URL that the app file (index.html) will be mounted on the server.
-  ROOT_PATH: "/app/"
-};
