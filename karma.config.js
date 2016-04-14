60524f7ca3d0e90e380d18d4683b243a339db980
Rick Carlino <rick.carlino@gmail.com>
2016-04-14 11:06:28 -0500

    Bring back the test suite

--- karma.config.js
+++ karma.config.js
@@ -5,57 +5,30 @@ module.exports = function (config) {
     singleRun: false,
     autoWatch: true,
     browsers: ['Chrome'],
-    coverageReporter: {
-      reporters: [
-        { type: 'html', subdir: 'html' },
-        { type: 'lcovonly', subdir: '.' },
-      ],
-    },
-    files: [
-      'tests.webpack.js',
-    ],
-    frameworks: [
-      'jasmine',
-    ],
+    files: [ 'tests.webpack.js' ],
+    frameworks: [ 'jasmine' ],
     preprocessors: {
       'tests.webpack.js': ['webpack', 'sourcemap'],
     },
-    reporters: ['progress', 'coverage'],
+    reporters: [ 'progress' ],
     webpack: {
+      resolve: {
+          extensions: ["", ".js", ".ts", ".tsx"]
+      },
       cache: true,
       devtool: 'inline-source-map',
       module: {
-        preLoaders: [
-          {
-            test: /-test\.js$/,
-            include: /src/,
-            exclude: /(bower_components|node_modules)/,
-            loader: 'babel',
-            query: {
-              cacheDirectory: true,
-            },
-          },
-          {
-            test: /\.js?$/,
-            include: /src/,
-            exclude: /(node_modules|bower_components|__tests__)/,
-            loader: 'babel-istanbul',
-            query: {
-              cacheDirectory: true,
-            },
-          },
-        ],
         loaders: [
           {
-            test: /\.js$/,
-            include: path.resolve(__dirname, '../src'),
-            exclude: /(bower_components|node_modules|__tests__)/,
-            loader: 'babel',
-            query: {
-              cacheDirectory: true,
-            },
-          },
+            test: /\.tsx?$/,
+            // include: path.resolve(__dirname, '../src'),
+            exclude: /(bower_components|node_modules)/,
+            loader: 'ts',
+          }
         ],
+      },
+      ts: {
+        configFileName: "tsconfig.json"
       },
     },
   });
