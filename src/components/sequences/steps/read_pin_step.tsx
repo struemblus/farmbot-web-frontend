e6c719a907c3cbafa65a06f713417e232a90963e
Rory Aronson <rory@roryaronson.com>
2016-04-30 17:24:45 -0700

    Add the remaining steps

---
+++ src/components/sequences/steps/read_pin_step.tsx
@@ -0,0 +1,104 @@
+import * as React from "react";
+import { changeStep, removeStep, pushStep } from "../sequence_actions";
+import { assign } from "lodash";
+import { Step, Sequence } from "../interfaces";
+
+interface CopyParams {
+  dispatch: Function;
+  step: Step;
+  index: number;
+}
+
+function copy({dispatch, step, index}: CopyParams) {
+  let copy = assign<{}, Step>({}, step);
+  dispatch(pushStep(copy, (index + 1)));
+};
+
+interface RemoveParams {
+  index: number;
+  dispatch: Function;
+}
+
+function remove({dispatch, index}: RemoveParams) {
+  dispatch(removeStep(index));
+}
+
+interface UpdateStepParams {
+  dispatch: Function;
+  step: Step;
+  index: number;
+  field: string; // "x"|"y"|"z"|"speed";
+}
+
+let updateStep = function ({ dispatch,
+                             step,
+                             index,
+                             field }: UpdateStepParams) {
+  return (e) => {
+    let update = assign<{}, Step>({}, step);
+    update.command[field] = e.target.value;
+    let action = changeStep(index, update);
+    dispatch(action);
+  };
+};
+
+interface IStepInputBox {
+  step: Step;
+  field: string;
+  dispatch: Function;
+  index: number;
+}
+function StepInputBox({step, field, dispatch, index}: IStepInputBox) {
+  return <input type="text"
+                value={ step.command[field] }
+                onChange={ updateStep({dispatch, step, index, field}) } />;
+}
+
+interface ReadPinStepParams {
+  dispatch: Function;
+  step: Step;
+  index: number;
+}
+export function ReadPinStep({dispatch,
+                                    step,
+                                    index}: ReadPinStepParams) {
+    return( <div>
+              <div className="step-wrapper">
+                <div className="row">
+                  <div className="col-sm-12">
+                    <div className="step-header read-pin-step">
+                      <h5>Read Pin</h5>
+                      <i className="fa fa-arrows-v step-control" />
+                      <i className="fa fa-clone step-control"
+                         onClick={ () => copy({dispatch, step, index}) } />
+                      <i className="fa fa-trash step-control"
+                         onClick={ () => remove({dispatch, index}) } />
+                      <i className="fa fa-angle-up step-control" />
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-sm-12">
+                    <div className="step-content read-pin-step">
+                      <div className="row">
+                        <div className="col-xs-6 col-md-3">
+                          <label>Pin Number</label>
+                          <StepInputBox dispatch={dispatch}
+                                        step={step}
+                                        index={index}
+                                        field="pin"/>
+                        </div>
+                        <div className="col-xs-6 col-md-3">
+                          <label>Data Label</label>
+                          <StepInputBox dispatch={dispatch}
+                                        step={step}
+                                        index={index}
+                                        field="label"/>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div> );
+}
--- src/components/sequences/steps/write_pin_step.tsx
+++ src/components/sequences/steps/write_pin_step.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-import { changeStep, removeStep, pushStep } from "./sequence_actions";
+import { changeStep, removeStep, pushStep } from "../sequence_actions";
 import { assign } from "lodash";
-import { Step, Sequence } from "./interfaces";
+import { Step, Sequence } from "../interfaces";
 
 interface CopyParams {
   dispatch: Function;
@@ -50,65 +50,57 @@ interface IStepInputBox {
 }
 function StepInputBox({step, field, dispatch, index}: IStepInputBox) {
   return <input type="text"
-                placeholder="0.123m"
                 value={ step.command[field] }
                 onChange={ updateStep({dispatch, step, index, field}) } />;
 }
 
-interface MoveRelativeStepParams {
+interface WritePinStepParams {
   dispatch: Function;
   step: Step;
   index: number;
 }
-export function MoveRelativeStep({dispatch,
+export function WritePinStep({dispatch,
                                     step,
-                                    index}: MoveRelativeStepParams) {
+                                    index}: WritePinStepParams) {
     return( <div>
-              <div className="action-wrapper">
+              <div className="step-wrapper">
                 <div className="row">
                   <div className="col-sm-12">
-                    <div className="action-header move-relative-action">
-                      <h5>Move Relative</h5>
-                      <i className="fa fa-arrows-v action-control" />
-                      <i className="fa fa-clone action-control"
+                    <div className="step-header write-pin-step">
+                      <h5>Write Pin</h5>
+                      <i className="fa fa-arrows-v step-control" />
+                      <i className="fa fa-clone step-control"
                          onClick={ () => copy({dispatch, step, index}) } />
-                      <i className="fa fa-trash action-control"
+                      <i className="fa fa-trash step-control"
                          onClick={ () => remove({dispatch, index}) } />
-                      <i className="fa fa-angle-up action-control" />
+                      <i className="fa fa-angle-up step-control" />
                     </div>
                   </div>
                 </div>
                 <div className="row">
                   <div className="col-sm-12">
-                    <div className="action-content move-relative-action">
+                    <div className="step-content write-pin-step">
                       <div className="row">
                         <div className="col-xs-6 col-md-3">
-                          <label>X</label>
+                          <label>Pin Number</label>
                           <StepInputBox dispatch={dispatch}
                                         step={step}
                                         index={index}
-                                        field="x"/>
+                                        field="pin"/>
                         </div>
                         <div className="col-xs-6 col-md-3">
-                          <label>Y</label>
+                          <label>Value</label>
                           <StepInputBox dispatch={dispatch}
                                         step={step}
                                         index={index}
-                                        field="y"/>
+                                        field="value"/>
                         </div>
                         <div className="col-xs-6 col-md-3">
-                          <label>Z</label>
+                          <label>Pin Mode</label>
                           <StepInputBox dispatch={dispatch}
                                         step={step}
                                         index={index}
-                                        field="z"/>
-                        </div>
-                        <div className="col-xs-6 col-md-3">
-                          <label>Speed</label>
-                          <StepInputBox dispatch={dispatch}
-                                        step={step}
-                                        index={index}
-                                        field="speed"/>
+                                        field="mode"/>
                         </div>
                       </div>
                     </div>
