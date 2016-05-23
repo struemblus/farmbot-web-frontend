04646eae02a22cf5df0959bb4f6b31480aa52e14
Rick Carlino <rick.carlino@gmail.com>
2016-05-23 10:35:15 -0500

    Middle menu can properly render data now

--- src/components/regimens/editor/regimen_item_list.tsx
+++ src/components/regimens/editor/regimen_item_list.tsx
@@ -1,42 +1,50 @@
 import * as React from "react";
 import { RegimenItem } from "../interfaces";
 import { Sequence } from "../../sequences/interfaces";
+import * as moment from "moment";
 import { duration } from "moment";
 
 interface RegimenItemDayGroupProps {
-  day: string;
-  items: RegimenItem[];
+    day: string;
+    items: RegimenItem[];
 }
 function RegimenItemDayGroup({ day, items }: RegimenItemDayGroupProps) {
     return <div className="regimen-day">
         <label>Day { day }</label>
-      { items.map((item, inx) => <RegimenItemStep sequence={ item.sequence } key={ inx }/>)}
+        { items.map(function(item, inx) {
+            return <RegimenItemStep item={ item } key={inx}/>;
+        }) }
     </div>;
 }
 
 interface RegimenItemStepProps {
-  sequence: Sequence;
+    item: RegimenItem;
 }
-function RegimenItemStep({sequence}: RegimenItemStepProps) {
+function RegimenItemStep({ item }: RegimenItemStepProps) {
+    let d = duration(item.timeOffset);
+    let time = moment({
+      hour: d.hours(),
+      minute: d.minutes()
+    }).format("h:mm a");
+
     return <div className="regimen-event">
-        <span className="regimen-event-title"> { sequence.name }</span>
-        <span className="regimen-event-time">10:30 AM</span>
+        <span className="regimen-event-title"> { item.sequence.name } </span>
+        <span className="regimen-event-time"> { time } </span>
         <i className="fa fa-trash regimen-control" />
     </div>;
 }
 
 interface RegimenItemListProps {
-  items: RegimenItem[];
+    items: RegimenItem[];
 }
 
 export function RegimenItemList({ items }) {
     let groups = _.groupBy<RegimenItem>(items, function(item: RegimenItem) {
-      return duration(item.timeOffset).days();
+        return duration(item.timeOffset).days();
     });
 
     let list = _.map(groups, function(innerItems, day) {
-      return <RegimenItemDayGroup day={ day } items={ innerItems } key={ day }/>;
+        return <RegimenItemDayGroup day={ day } items={ innerItems } key={ day }/>;
     });
-
     return <div> { list } </div>
 }
