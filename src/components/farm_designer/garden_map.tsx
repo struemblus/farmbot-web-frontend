621434b48a9a2064f639bcf91a9770af236fa096
Rick Carlino <rick.carlino@gmail.com>
2016-05-27 16:53:27 -0500

    Resurect the Farm Design page

--- src/components/farm_designer/garden_map.tsx
+++ src/components/farm_designer/garden_map.tsx
@@ -1,7 +1,5 @@
-import * as React from 'react';
-import { Plant } from '../../models/plant';
-import { navigateSelectedPlant } from '../../actions/router_actions';
-import { getParam } from '../../util.ts';
+import * as React from "react";
+import { navigateSelectedPlant } from "../../actions/router_actions";
 
 export class MapPoint extends React.Component<any, any> {
   select() {
@@ -13,7 +11,7 @@ export class MapPoint extends React.Component<any, any> {
   }
 
   render() {
-    let length = this.props.global.planting_area.length;
+    let length = 240; // this.props.global.planting_area.length;
     let fill = this.selected() ? "red" : "black";
     return <circle cx={ this.props.plant.x }
                    cy={ (-1 * this.props.plant.y) + length - 30 }
@@ -27,25 +25,26 @@ export class GardenMap extends React.Component<any, any> {
   plants() {
     return this.props.plants.all.map(
       function (p, k) {
-        let selected = (getParam("plant") === p._id);
         return <MapPoint plant={ p }
                   key={ k }
-                  { ...this.props }/>
+                  { ...this.props }/>;
       }.bind(this)
     );
   }
 
   render() {
     let style = {
-      fill:        'rgba(0,0,0,0.05)',
+      fill:        "rgba(0,0,0,0.05)",
       strokeWidth: 5,
-      stroke:      'rgba(0,0,0,0.15)'
-    }
+      stroke:      "rgba(0,0,0,0.15)"
+    };
 
-    let {width, length} = this.props.global.planting_area;
+    let width = length = 240; // this.props.global.planting_area;
 
     return <div>
-             <div className="drop-area" id="drop-area" style={ {marginLeft: '10px', marginTop: '10px'} }>
+             <div className="drop-area"
+                  id="drop-area"
+                  style={ {marginLeft: "10px", marginTop: "10px"} }>
               <svg width={ width }
                    height={ length } >
                 <rect width={ width }
