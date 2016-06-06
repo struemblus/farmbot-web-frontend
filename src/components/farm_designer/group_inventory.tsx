24936e707a6c7de6d830471f16c1b17d233ce40d
Rick Carlino <rick.carlino@gmail.com>
2016-06-06 09:32:05 -0500

    Monday

--- src/components/farm_designer/group_inventory.tsx
+++ src/components/farm_designer/group_inventory.tsx
@@ -7,7 +7,10 @@ export class Groups extends React.Component<any, any> {
       <div className="panel-container cyan-panel">
         <div className="panel-header cyan-panel">
           <div className="main-nav-button">
-            <button className="navbar-toggle hidden-sm hidden-md hidden-lg" data-target="#navbar" data-toggle="collapse" type="button">
+            <button className="navbar-toggle hidden-sm hidden-md hidden-lg"
+                    data-target="#navbar"
+                    data-toggle="collapse"
+                    type="button">
               <span className="glyphicon glyphicon-menu-hamburger" />
             </button>
           </div>
--- src/components/farm_designer/plant_inventory.tsx
+++ src/components/farm_designer/plant_inventory.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { Link } from 'react-router';
-import { Plant } from '../../models/plant'
+import { Link } from "react-router";
+import { Plant } from "../../models/plant";
 
 export class Plants extends React.Component<any, any> {
   render() {
@@ -8,7 +8,10 @@ export class Plants extends React.Component<any, any> {
       <div className="panel-container green-panel">
         <div className="panel-header green-panel">
           <div className="main-nav-button">
-            <button className="navbar-toggle hidden-sm hidden-md hidden-lg" data-target="#navbar" data-toggle="collapse" type="button">
+            <button className="navbar-toggle hidden-sm hidden-md hidden-lg"
+                    data-target="#navbar"
+                    data-toggle="collapse"
+                    type="button">
               <span className="glyphicon glyphicon-menu-hamburger" />
             </button>
           </div>
@@ -18,7 +21,8 @@ export class Plants extends React.Component<any, any> {
                   <Link to={ "/dashboard/designer?p1=NoTab" }>Designer</Link>
               </li>
               <li>
-              <Link to={ { pathname: "/dashboard/designer", query: { p1: "Plants" }} } className={"active"}>
+              <Link to={ { pathname: "/dashboard/designer", query: { p1: "Plants" }} }
+                    className={"active"}>
                 Plants
               </Link>
               </li>
@@ -28,12 +32,17 @@ export class Plants extends React.Component<any, any> {
                 </Link>
               </li>
               <li>
-              <Link to={ { pathname: "/dashboard/designer", query: { p1: "Zones" }} } className={"active"}>
+              <Link to={ { pathname: "/dashboard/designer", query: { p1: "Zones" }} }
+                    className={"active"}>
                 Zones
               </Link>
               </li>
               <li className="hidden-sm hidden-md hidden-lg">
-                  <Link to={ "/dashboard/designer?p1=Panel2" }>Calendar</Link>
+                  <Link to={ {
+                    pathname: "/dashboard/designer",
+                    query: { p1: "Panel2" }} }>
+                      Calendar
+                  </Link>
               </li>
             </ul>
           </div>
@@ -48,7 +57,9 @@ export class Plants extends React.Component<any, any> {
           </div>
         </div>
         <Link to="/dashboard/designer?p1=SpeciesCatalog">
-          <div className="plus-button add-plant button-like" data-toggle="tooltip" title="Add plant">
+          <div className="plus-button add-plant button-like"
+               data-toggle="tooltip"
+               title="Add plant">
             <i className="fa fa-2x fa-plus" />
           </div>
         </Link>
