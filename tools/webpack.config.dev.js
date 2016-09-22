740601b680f215e97bac2b73c887c832f678547b
Rick Carlino <rick.carlino@gmail.com>
2016-09-22 11:32:21 -0500

    default.html --> index.html

--- tools/webpack.config.dev.js
+++ tools/webpack.config.dev.js
@@ -25,6 +25,6 @@ module.exports = {
     extensions: [ '', '.js', '.ts', '.tsx' ],
   },
   devServer: {
-    historyApiFallback: { index: '/app/default.html' }
+    historyApiFallback: { index: '/app/index.html' }
   }
 };
