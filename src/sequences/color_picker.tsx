d96ebfb2088df7b6ccef486732b551e53f592cfa
Rick Carlino <rick.carlino@gmail.com>
2016-09-27 14:41:59 -0500

    Hoverable color picker.

--- src/sequences/color_picker.tsx
+++ src/sequences/color_picker.tsx
@@ -3,18 +3,35 @@ import { Saucer } from "../ticker/saucer";
 import { Color } from "../interfaces";
 import { colors } from "../util";
 
-export interface ColorPickerProps {
-    current: Color;
-}
+interface ColorPickerProps { current: Color; }
+interface ColorPickerState { isHovered: boolean; }
 
-export function ColorPicker({ current }: ColorPickerProps) {
-    return (<div>
-        <Saucer color={ current } />
-    </div>);
-}
+export class ColorPicker extends React.Component<ColorPickerProps, ColorPickerState> {
 
-// <div className="help-text">
-//     {
-//         colors.map(c => <Saucer color={c} />)
-//     }
-// </div>
+  constructor(props: ColorPickerProps) {
+    super(props);
+    this.state = { isHovered: false };
+  }
+
+  /** The output of this function is what the user will see when the
+    color circle is hovered over. */
+  isHovered({text}: { text: string }) {
+    return <div>
+      {
+        colors.map((c, i) => <Saucer color={c} key={i} />)
+      }
+    </div>;
+  }
+
+  /** This is what the user sees when the circle is not hovered over. */
+  notHovered(props: ColorPickerProps) { return <div></div>; }
+
+  render() {
+    let Comp = (this.state.isHovered ? this.isHovered : this.notHovered);
+    return <div onMouseEnter={() => { this.setState({ isHovered: true }); } }
+                onMouseLeave={() => { this.setState({ isHovered: false }); } } >
+      <Saucer color={this.props.current} />
+      <Comp />
+    </div>;
+  };
+}
