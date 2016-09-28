aa7e94e10724b2d3c51a2ee55a8c26178871de08
Rick Carlino <rick.carlino@gmail.com>
2016-09-28 08:45:17 -0500

    <ColorPicker/> tests :heart: Enzyme

--- src/__tests__/sequences/color_picker_spec.tsx
+++ src/__tests__/sequences/color_picker_spec.tsx
@@ -2,10 +2,10 @@ import { ColorPicker } from "../../sequences/color_picker";
 import { Saucer } from "../../ticker/saucer";
 import { colors } from "../../util";
 import * as React from "react";
-import { render } from "enzyme";
+import { mount } from "enzyme";
 
 describe("Color picker", () => {
-  it("initializes", function() {
+  it("initializes.", function() {
     let colpik = new ColorPicker({current: "red"});
     expect(colpik.state.isHovered).toBeFalsy(
         "ColorPicker.state.isHovered should initialize to 'false'.");
@@ -17,17 +17,27 @@ describe("Color picker", () => {
     expect(actualColors).toEqual(colors.sort());
   });
 
-  it("picks a color", () => {
+  it("picks colors.", () => {
   // Render
     let callback = jasmine.createSpy("<ColorPicker/> onChange property");
-    let huh = render(<ColorPicker current="red" />);
+    let el = mount(<ColorPicker current="red" onChange={ callback }/>);
 
+  function saucerCount() { return el.find(".saucer").length; }
   // Unhovered
-      debugger;
+    expect(saucerCount()).toEqual(1,
+      "Color picker shows 1 color saucer when unhovered.");
 
   // hover
+    el.simulate("mouseEnter");
+    expect(saucerCount()).toEqual((colors.length + 1),
+      "Color picker shows all possible colors (plus current choice) when unhovered.");
 
   // Select a color
+    let saucer = el.find(".saucer").last();
+    let color = (saucer.props().style as any)["background"];
+    saucer.simulate("click");
+    expect(callback).toHaveBeenCalledWith(color);
 
+  // unhover
   });
 });
