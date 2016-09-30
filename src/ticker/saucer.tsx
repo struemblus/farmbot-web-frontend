91e86621a9eda5719e90ca52e3118cebafb87f63
Rory Aronson <rory@roryaronson.com>
2016-09-29 19:45:42 -0700

    style tweaks to the nav active link and the color picker

--- src/ticker/saucer.tsx
+++ src/ticker/saucer.tsx
@@ -10,7 +10,7 @@ export interface SaucerProps {
 export /** A colored UI disc/circle. */
   function Saucer({ color, border, style }: SaucerProps) {
     style = defensiveClone<{[k: string]: string}>(style || {});
-    _.assign(style, { transition: "all 0.5s ease", background: (color || "gray") });
+    _.assign(style, { transition: "all 0.2s ease", background: (color || "gray") });
     if (border) { (style || {})["border"] = `3px solid ${ border }`; }
     return <div className="saucer" style={ style } />;
 }
