ae4292745eb0144488178493e80512cc1b2bb581
Rick Carlino <rick.carlino@gmail.com>
2016-10-10 09:17:36 -0500

    Show 0s in the form instead of empty string

--- src/sequences/step_tiles.tsx
+++ src/sequences/step_tiles.tsx
@@ -72,7 +72,9 @@ interface IStepInput {
 }
 
 export function StepInputBox({step, field, dispatch, index}: IStepInput) {
-    let val = (step.args as any )[field] || "";
+    let raw = (step.args as any )[field];
+    let notUndefied = (_.isString(raw) || _.isNumber(raw));
+    let val = notUndefied ? raw : "";
 
     return <input type="text"
               value={ val }
