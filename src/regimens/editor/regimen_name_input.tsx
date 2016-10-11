a61fcb4aa34e15fc594641f1348f6b140c614290
Rick Carlino <rick.carlino@gmail.com>
2016-10-11 15:55:11 -0500

    Ability to change regimen color

--- src/regimens/editor/regimen_name_input.tsx
+++ src/regimens/editor/regimen_name_input.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { editRegimen } from "../actions";
 import { RegimenProps, Regimen } from "../interfaces";
 import { t } from "i18next";
+import { ColorPicker } from "../../sequences/color_picker";
+import { Color } from "../../interfaces";
 
 function write({dispatch, regimen}: RegimenProps): React.EventHandler<React.FormEvent> {
   if (!regimen) {
@@ -18,11 +20,34 @@ function write({dispatch, regimen}: RegimenProps): React.EventHandler<React.Form
   };
 }
 
+function updateColor({dispatch, regimen}: RegimenProps) {
+  if (!regimen) {
+    throw new Error("Regimen is required");
+  } else {
+    return (function colorUpdater(color: Color) {
+      dispatch(editRegimen(regimen, { color }));
+    });
+  }
+};
 export function RegimenNameInput({regimen, dispatch}: RegimenProps) {
   let value = (regimen && regimen.name) || "";
-  return <input id="right-label"
-    placeholder= {t("Regimen Name")}
-    type="text"
-    onChange={ write({ dispatch, regimen }) }
-    value={ value }/>;
+  // return <input id="right-label"
+  //   placeholder= {t("Regimen Name")}
+  //   type="text"
+  //   onChange={ write({ dispatch, regimen }) }
+  //   value={ value }/>;
+  return (<div className="row">
+    <div className="col-sm-10">
+      <input id="right-label"
+        placeholder={t("Regimen Name")}
+        type="text"
+        onChange={write({ dispatch, regimen })}
+        value={value} />
+    </div>
+    <div className="col-sm-1">
+      <ColorPicker current={(regimen && regimen.color) || "gray"}
+        onChange={updateColor({ dispatch, regimen })} />
+    </div>
+    <div className="col-sm-1" />
+  </div>);
 }
