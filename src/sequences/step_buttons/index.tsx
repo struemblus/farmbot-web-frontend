def75510d5d8b011c6fa8fa683df9317dde6a1c4
Rick Carlino <rick.carlino@gmail.com>
2016-10-24 16:57:32 -0500

    Ghost image when dragging a tile

--- src/sequences/step_buttons/index.tsx
+++ src/sequences/step_buttons/index.tsx
@@ -11,22 +11,17 @@ export function StepButton({children, onClick, color}: StepButtonParams) {
     let classes = `full-width text-left ${color}-block block-header block`;
 
     function undrag(ev: React.DragEvent) {
-        (ev.target as HTMLElement).classList.remove("hey-rory");
+        document.querySelector("#temp-drag-and-drop").remove();
     }
 
     function drag(ev: React.DragEvent) {
         let key = nastyStorargeSet(onClick || badRef);
-        (ev.target as HTMLElement).classList.add("hey-rory");
-        // var crt = (ev.target as HTMLElement).cloneNode(true);
-        // /* or visibility: hidden, or any of the above */
-        // crt.style.transform = "rotate(45deg)";
-        // crt.style.position = "absolute";
-        // crt.style.top = "-150px";
-        // crt.style.display = "inline block";
-        // crt.style.width = "123";
-        // document.body.appendChild(crt);
-        // (ev.dataTransfer as any).setDragImage(crt, 0, 0);
-
+        var el = (ev.target as HTMLElement).cloneNode(true) as HTMLElement;
+        el.classList.add("hey-rory");
+        el.style.top = "-99999px";
+        el.id = "temp-drag-and-drop";
+        document.body.appendChild(el);
+        (ev.dataTransfer as any).setDragImage(el, 0, 0);
         ev.dataTransfer.setData("text", key);
     }
 
