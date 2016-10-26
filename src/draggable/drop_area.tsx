9d3bcfaacfa8a5e9de69ff99f276bb7b9473aedf
Rick Carlino <rick.carlino@gmail.com>
2016-10-26 07:52:06 -0500

    [UNSTABLE!!] Fix terrible idea.

--- src/draggable/drop_area.tsx
+++ src/draggable/drop_area.tsx
@@ -1,44 +1,3 @@
-// export class DropArea extends React.Component<DropAreaProps, DropAreaState> {
-//     constructor() {
-//         super();
-//         this.state = {
-//             isHovered: false
-//         };
-//     }
-
-//     toggle() {
-//         this.setState({ isHovered: !this.state.isHovered });
-//     }
-
-//     dragOver(event: React.DragEvent) {
-//         event.preventDefault();
-//         this.toggle();
-//     }
-
-//     drop(event: React.DragEvent) {
-//         event.preventDefault();
-//         console.log("?????");
-//         if (this.props.onDrop) {
-//             console.log("?????");
-//             this.props.onDrop(event.dataTransfer.getData("text"));
-//         }
-//     }
-
-//     render() {
-//         let isVisible = this.props.isLocked || this.state.isHovered;
-//         if (isVisible) {
-//             return <div className="drag-drop-area padding"
-//                 onDrop={this.drop.bind(this)}
-//                 onDragLeave={this.toggle.bind(this)}>
-//                 {this.props.children}
-//             </div>;
-//         } else {
-//             return <div style={INVISIBLE_STYLE}
-//                 onDragOver={this.dragOver.bind(this)} />;
-//         }
-//     }
-// }
-
 import { DropAreaProps, DropAreaState } from "./interfaces";
 import { stepGet } from "./actions";
 import * as React from "react";
@@ -59,41 +18,39 @@ export class DropArea extends React.Component<DropAreaProps, DropAreaState> {
     }
 
     dragOver(event: any) {
+        console.log("Dragover");
         event.preventDefault();
     }
 
     drop(event: React.DragEvent) {
+        console.log("Drop");
         event.preventDefault();
-        console.log("Drawwwp");
         let key = event.dataTransfer.getData("text");
         let fn = this.props.callback;
         if (fn) { fn(key); }
     }
 
     toggle() {
+        console.log("Toggle");
         this.setState({ isHovered: !this.state.isHovered });
     }
 
     render() {
         let isVisible = this.props.isLocked || this.state.isHovered;
-        // return <div className="drag-drop-area padding"
-        //     onDragOver={preventDefault}
-        //     onDrop={this.drop.bind(this)}
-        //     hidden={!isVisible}>
-        //     {this.props.children}
-        // </div>;
 
-        if (isVisible) {
+        if (true/*isVisible*/) {
             return <div className="drag-drop-area padding"
-                onDragEnter={this.toggle.bind(this)}
-                onDragLeave={this.toggle.bind(this)}>
+                onDragOver={this.dragOver.bind(this)}
+                onDrop={this.drop.bind(this)}>
                 {this.props.children}
             </div>;
         } else {
-            return <div style={INVISIBLE_STYLE}
-                onDragLeave={this.toggle.bind(this)}
-                onDragEnter={this.toggle.bind(this)}
-                onDragOver={this.dragOver.bind(this)} />;
+            //     return <div style={INVISIBLE_STYLE}
+            //         onDragLeave={this.toggle.bind(this)}
+            //         onDragEnter={this.toggle.bind(this)}
+            //         onDragOver={this.dragOver.bind(this)}>
+            //         WHatever </div>;
+            return <div />;
         }
 
     }
