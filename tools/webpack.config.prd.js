df14eb13b2e4d48605ca5c42e638b438db3b0f83
Rick Carlino <rick.carlino@gmail.com>
2016-10-27 09:05:36 -0500

    Nicer revision info

--- tools/webpack.config.prd.js
+++ tools/webpack.config.prd.js
@@ -13,7 +13,7 @@ c = function () {
     .push(new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
       'process.env.REVISION': JSON.stringify(
-        exec('git log --pretty=format:"%h" -1').toString()),
+        exec('git log --pretty=format:"%h%n%ad%n%f" -1').toString()),
 
     }));
 
