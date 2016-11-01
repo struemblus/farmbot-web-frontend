3546c422c456c7a2dbed47c296cf56c7e8783d4e
Rick Carlino <rick.carlino@gmail.com>
2016-11-01 16:36:42 -0500

    First person ticker messages

--- src/ticker/reducer.ts
+++ src/ticker/reducer.ts
@@ -3,11 +3,23 @@ import { TickerState } from "./interfaces";
 import { ReduxAction } from "../interfaces";
 import { RpcBotLog } from "../devices/interfaces";
 import * as i18next from "i18next";
+import { store } from "../store";
 
 let YELLOW = "#fd6",
     RED = "#e66",
     GREEN = "#6a4",
     BLUE = "#4286f4";
+
+function firstPerson(color: string, message: string, show = true) {
+    return function (s: TickerState, a: ReduxAction<{}>) {
+        // TODO: HACK: This is such a hack, but I can't think of a
+        // faster way to get it done and it's not a very critical part of the
+        // system. PRs welcome. Help appreciated. In the meantime, #shipIt
+        let n = _.get<string>(store.getState(), "bot.account.name", "Bot");
+        message = i18next.t(`${n} is ${message}`);
+        return { color, message, show };
+    };
+}
 
 function change(color: string, message: string, show = true) {
     return function (s: TickerState, a: ReduxAction<{}>) {
@@ -21,12 +33,12 @@ export let tickerReducer = generateReducer<TickerState>({
     color: "gray",
     show: true
 })
-    .add<{}>("LOGIN_OK", change(YELLOW, "Logged in"))
+    .add<{}>("LOGIN_OK", firstPerson(YELLOW, "logged in."))
     .add<{}>("LOGIN_ERR", change(RED, "Bad login"))
-    .add<{}>("FETCH_PLANTS_START", change(YELLOW, "Fetching plants"))
+    .add<{}>("FETCH_PLANTS_START", firstPerson(YELLOW, "fetching plants."))
     .add<{}>("FETCH_PLANTS_OK", change(GREEN, "Done fetching plants"))
-    .add<{}>("FETCH_PLANTS_ERR", change(RED, "Error fetching plants"))
-    .add<{}>("FETCH_SEQUENCES_OK", change(GREEN, "Done fetching sequences"))
+    .add<{}>("FETCH_PLANTS_ERR", firstPerson(RED, "unable to fetch plants."))
+    .add<{}>("FETCH_SEQUENCES_OK", firstPerson(GREEN, "done fetching sequences."))
     .add<{}>("FETCH_DEVICE_ERR", change(RED, "Can't connect to MQTT server"))
     .add<{}>("BOT_SYNC_OK", change(GREEN, "Bot Status OK"))
     .add<string>("BOT_ERROR", (s, a) => {
