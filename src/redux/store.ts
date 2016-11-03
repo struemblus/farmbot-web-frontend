e0a619ed4ead6ed6a2072eef24ca162fb3fc2b05
Rick Carlino <rick.carlino@gmail.com>
2016-11-03 07:28:55 -0500

    Clean up store some more

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,36 +1,36 @@
 import thunk from "redux-thunk";
 import { createStore, applyMiddleware, compose } from "redux";
-import { dontExitIfBrowserIsOnHold } from "../browser_holds/index";
 import { Store } from "./interfaces";
 import { rootReducer } from "./root_reducer";
+import { registerSubscribers } from "./subscribers";
 
 let ENV = process.env.NODE_ENV as string;
 
+/** Business logic that creates the Redux store in dev env.
+ * Most of this is adding dev tools and other helpers. */
 function dev() {
-  let lastState = JSON.parse(sessionStorage["lastState"] || "{}");
-  let dt = (window as any)["devToolsExtension"];
-  let risi = require("redux-immutable-state-invariant")();
-  let srsly = compose(applyMiddleware(thunk, risi),
-    dt ? dt() : (f: any) => f);
-  store = createStore(rootReducer, lastState, srsly);
-  // Make store global in dev env in case I need to probe it.
-  (window as any)["store"] = store;
-  store.subscribe(function () {
-    sessionStorage["lastState"] = JSON.stringify(store.getState());
-  });
-  return store;
+    let lastState = JSON.parse(sessionStorage["lastState"] || "{}");
+    let dt = (window as any)["devToolsExtension"];
+    let risi = require("redux-immutable-state-invariant")();
+    let srsly = compose(applyMiddleware(thunk, risi),
+        dt ? dt() : (f: any) => f);
+    store = createStore(rootReducer, lastState, srsly);
+    // Make store global in dev env in case I need to probe it.
+    (window as any)["store"] = store;
+    store.subscribe(function() {
+        sessionStorage["lastState"] = JSON.stringify(store.getState());
+    });
+    return store;
 }
 
 function prod() {
-  return createStore(rootReducer, {}, applyMiddleware(thunk));
+    return createStore(rootReducer, {}, applyMiddleware(thunk));
 }
 
-
-
 function configureStore(options = {}) {
-  let store: Store = (ENV === "production" ? prod() : dev());
-  dontExitIfBrowserIsOnHold(store);
-  return store;
+    let store: Store = (ENV === "production" ? prod() : dev());
+    registerSubscribers(store);
+    return store;
 }
 
 export let store = configureStore();
---
+++ src/redux/subscribers.ts
@@ -0,0 +1,32 @@
+import { Everything } from "../interfaces";
+import { Store } from "./interfaces";
+import { dontExitIfBrowserIsOnHold } from "../browser_holds/index";
+
+interface Subscription {
+    fn: (state: Everything) => void;
+    env: "production" | "development" | "*";
+};
+
+/** To make it easier to manage all things watching the state tree,
+ * we keep subscriber functions in this array. */
+export let subscriptions: Subscription[] = [
+    {
+        env: "development",
+        fn: function storeState(state: Everything) {
+            sessionStorage["lastState"] = JSON.stringify(state);
+        }
+    }
+    , {
+        env: "*",
+        fn: dontExitIfBrowserIsOnHold
+    }
+];
+
+export function registerSubscribers(store: Store) {
+    let ENV_LIST = [process.env.NODE_ENV, "*"];
+    subscriptions.forEach(function (s) {
+        if (ENV_LIST.includes(s.env)) {
+            store.subscribe(() => s.fn(store.getState()));
+        };
+    });
+};
