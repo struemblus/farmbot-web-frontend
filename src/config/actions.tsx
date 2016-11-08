3c9a041e2da9ccd6515c53e0afa7fd54a4a3e95d
Rick Carlino <rick.carlino@gmail.com>
2016-11-08 17:08:30 -0600

    Factor down URL parsing and `ready()`.

--- src/config/actions.tsx
+++ src/config/actions.tsx
@@ -2,6 +2,7 @@ import { didLogin } from "../auth/actions";
 import { AuthState } from "../auth/interfaces";
 import { ReduxAction, Thunk } from "../redux/interfaces";
 import { ChangeApiHost, ChangeApiPort } from "./interfaces";
+import { API } from "../api";
 
 export function changeApiHost(host: string): ReduxAction<ChangeApiHost> {
   return { type: "CHANGE_API_HOST", payload: { host } };
@@ -14,23 +15,12 @@ export function changeApiPort(port: string): ReduxAction<ChangeApiPort> {
 /** Lets Redux know that the app is ready to bootstrap. */
 export function ready(): Thunk {
   return (dispatch, getState) => {
-    let state: AuthState;
-    // Old state is sometimes stored in localStorage for hot reloading pruposes.
-    let token: string | undefined = localStorage["token"];
-    if (token) {
-      // Grab cached state from localStorage if it exists.
-      state = JSON.parse(token);
-    } else {
-      state = getState().auth || {};
-    }
-    // HACK: If state.token is longer than 30 characters, we know it is there
-    // and not a stub. This is a leftover from when Typescript had not-so-great
-    // support for nullable types. TODO: Make state.auth.token a nullable type.
-    if ((state.token || "").length > 30) { // lol
-      didLogin(state, dispatch);
-      return { type: "READY_HAD_TOKEN", payload: {} };
-    } else {
-      return { type: "READY_NO_TOKEN", payload: {} };
-    };
+    debugger;
+    /** Token that may or may not be stored in localStorage from previous
+     * user session. */
+    let t: string | undefined = localStorage["token"];
+    let state: AuthState = t ? JSON.parse(t) : (getState().auth || {});
+    let hasToken = state.token && state.token.length > 30;
+    if (hasToken) { didLogin(state, dispatch); };
   };
 }
