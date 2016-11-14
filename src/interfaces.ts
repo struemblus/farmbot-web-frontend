55f31cdcea4b5e3f796ad3893f5f868436046b30
Rick Carlino <rick.carlino@gmail.com>
2016-11-14 13:56:59 -0600

    Write a sync object interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,14 +1,14 @@
-import { AuthState } from "./auth/interfaces";
+import { AuthState, User } from "./auth/interfaces";
 import { ConfigState } from "./config/interfaces";
-import { BotState } from "./devices/interfaces";
+import { BotState, DeviceAccountSettings } from "./devices/interfaces";
 import { TickerState } from "./ticker/interfaces";
 import { BulkSchedulerState } from "./regimens/bulk_scheduler/interfaces";
-import { RegimensState } from "./regimens/interfaces";
-import { SequenceReducerState } from "./sequences/interfaces";
-import { DesignerState } from "./farm_designer/interfaces";
+import { RegimensState, RegimenApiResponse, RegimenItem } from "./regimens/interfaces";
+import { SequenceReducerState, Sequence } from "./sequences/interfaces";
+import { DesignerState, Plant } from "./farm_designer/interfaces";
 import { Color as FarmBotJsColor } from "farmbot/dist/interfaces";
 import { DragableState } from "./draggable/interfaces";
-import { PeripheralState } from "./controls/peripherals/interfaces";
+import { PeripheralState, Peripheral } from "./controls/peripherals/interfaces";
 
 /** Regimens and sequences may have a "color" which determines how it looks
     in the UI. Only certain colors are valid. */
@@ -47,5 +47,16 @@ export interface Everything {
 
 /** A sync object, as returned by a GET request to `/api/sync` */
 export interface Sync {
-
+  /** Git commit hash of the revision that the server is running. */
+  api_version: string;
+  /** A number that, when incremented, alerts the bot that
+   * its software is out of date */
+  compat_num: number;
+  device: DeviceAccountSettings;
+  users: User;
+  sequences: Sequence[];
+  regimens: RegimenApiResponse[];
+  peripherals: Peripheral[];
+  regimen_items: RegimenItem;
+  plants: Plant;
 }
