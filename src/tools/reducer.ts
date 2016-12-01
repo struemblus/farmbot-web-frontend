c1f45ac67b8abdd8354a795f643a55b9d878be24
MrChristofferson <chrisvan0848@gmail.com>
2016-12-01 08:57:30 -0700

    delete unnecessary method

--- src/tools/reducer.ts
+++ src/tools/reducer.ts
@@ -17,41 +17,40 @@ let initialState: ToolsState = {
 };
 
 export let toolsReducer = generateReducer<ToolsState>(initialState)
-    .add<Sync>("FETCH_SYNC_OK", function(s, a) {
-        let state = _.cloneDeep(s);
-        state.tool_bays = a.payload.tool_bays || [];
-        state.tool_slots = a.payload.tool_slots || [];
-        state.tools = a.payload.tools || [];
-        return state;
+    .add<Sync>("FETCH_SYNC_OK", function (s, a) {
+        s.tool_bays = a.payload.tool_bays || [];
+        s.tool_slots = a.payload.tool_slots || [];
+        s.tools = a.payload.tools || [];
+        return s;
     })
-    .add<{}>("EDIT_TOOLS_START", function(s, a) {
+    .add<{}>("EDIT_TOOLS_START", function (s, a) {
         s.editorMode = true;
         return s;
     })
-    .add<{}>("EDIT_TOOLS_STOP", function(s, a) {
+    .add<{}>("EDIT_TOOLS_STOP", function (s, a) {
         s.editorMode = false;
         return s;
     })
-    .add<{ slot_id: number }>("DESTROY_SLOT", function(s, a) {
+    .add<{ slot_id: number }>("DESTROY_SLOT", function (s, a) {
         let { tool_slots } = s;
         let index = _.findIndex(tool_slots, { id: a.payload.slot_id });
         tool_slots.splice(index, 1);
         return s;
     })
-    .add<UpdateToolSlotPayl>("UPDATE_SLOT", function(s, a) {
+    .add<UpdateToolSlotPayl>("UPDATE_SLOT", function (s, a) {
         let { slot_id, property, value } = a.payload;
         let slot = _.findWhere(s.tool_slots, { id: parseInt(slot_id) });
         /** ??? TODO: Tried changing interfaces but can't seem to please TS */
         (slot as any)[property] = parseInt(value);
         return s;
     })
-    .add<ToolBay>("SAVE_TOOL_BAY_NAME_OK", function(s, a) {
+    .add<ToolBay>("SAVE_TOOL_BAY_NAME_OK", function (s, a) {
         let { id, name } = a.payload;
         let bay = _.findWhere(s.tool_bays, { id });
         bay.name = name;
         return s;
     })
-    .add<AddToolSlotPayl>("ADD_SLOT", function(s, a) {
+    .add<AddToolSlotPayl>("ADD_SLOT", function (s, a) {
         let { payload } = a;
         let { slotState } = payload;
         s.tool_slots.push({
@@ -63,13 +62,13 @@ export let toolsReducer = generateReducer<ToolsState>(initialState)
         });
         return s;
     })
-    .add<{ tool_id: number }>("DESTROY_TOOL", function(s, a) {
+    .add<{ tool_id: number }>("DESTROY_TOOL", function (s, a) {
         let { tools } = s;
         let index = _.findIndex(tools, { id: a.payload.tool_id });
         tools.splice(index, 1);
         return s;
     })
-    .add<ToolPayl>("ADD_TOOL", function(s, a) {
+    .add<ToolPayl>("ADD_TOOL", function (s, a) {
         let { name, slot_id, id } = a.payload;
         s.tools.push({ id, name, slot_id });
         return s;
