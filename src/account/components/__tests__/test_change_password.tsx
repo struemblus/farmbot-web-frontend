6811453de77744b8c84abcd47ba04f56c6a13e97
Rick Carlino <rick.carlino@gmail.com>
2016-12-09 09:24:57 -0600

    npm test

--- src/account/components/__tests__/test_change_password.tsx
+++ src/account/components/__tests__/test_change_password.tsx
@@ -1,7 +1,18 @@
+import * as React from "react";
+import { shallow } from "enzyme";
 import { ChangePassword } from "../change_password";
+import { ChangePwPropTypes } from "../../interfaces";
 
-test("wow", function () {
-    it("such TDD", function () {
-        expect("Many stability").toBeTruthy();
+test("<ChangePassword/>", function() {
+    it("renders", function() {
+        let props: ChangePwPropTypes = {
+            password: "wow",
+            new_password: "wow",
+            new_password_confirmation: "wow",
+            set: function() { },
+            save: function() { }
+        };
+        let dom = shallow(<ChangePassword {...props} />);
+        expect(dom.text()).toContain("password");
     });
 });
