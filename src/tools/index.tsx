b717de77a07444bc41dcf8448fdb3126a472217a
MrChristofferson <chrisvan0848@gmail.com>
2016-12-11 21:40:08 -0700

    remove tools WIP message

--- src/tools/index.tsx
+++ src/tools/index.tsx
@@ -4,16 +4,9 @@ import { ToolsState } from "./interfaces";
 import { ToolBayList, ToolBayForm, ToolList, ToolForm } from "./components";
 import { connect } from "react-redux";
 import { Page, Row, Col } from "../ui";
-import { success } from "../ui";
 
 @connect((state: Everything) => state)
 export class Tools extends React.Component<Everything, ToolsState> {
-    componentDidMount() {
-        success(
-            "Subscribe to the FarmBot.io mailing list for news and updates.",
-            "Work in Progress"
-        );
-    }
     render() {
         let editing = this.props.tools.editorMode;
         let isEditingTools = this.props.tools.tools.isEditing;
