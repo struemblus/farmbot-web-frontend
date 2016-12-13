fc8cc0ce82db92c64ce6cad76b7a02fcbb498053
MrChristofferson <chrisvan0848@gmail.com>
2016-12-13 10:00:45 -0700

    documentation

--- src/app.tsx
+++ src/app.tsx
@@ -6,15 +6,15 @@ import { init } from "./ui";
 init();
 /** For external device debugging purposes */
 // let script = document.createElement("script");
-// script.src = "http://YOUR-IP/target/target-script-min.js#anonymous";
+// script.src = "http://YOUR-IP:8081/target/target-script-min.js#anonymous";
 // document.body.appendChild(script);
 export default class App extends React.Component<Everything, {}> {
-  render() {
-    return (
-      <div className="app">
-        <NavBar { ...this.props } />
-        {this.props.children}
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app">
+                <NavBar { ...this.props } />
+                {this.props.children}
+            </div>
+        );
+    }
 }
