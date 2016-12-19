2e2a88436d5d2d6a5ca865d32d295c27e9d8cca6
Rick Carlino <rick.carlino@gmail.com>
2016-12-19 13:14:21 -0600

    Unbreak CI (sorry)

--- src/nav/__tests__/test_nav_links.ts
+++ src/nav/__tests__/test_nav_links.ts
@@ -1,29 +1,30 @@
-// import * as React from "react";
-// import { mount } from "enzyme";
-// import { NavBar } from "../index";
-// import { Everything } from "../../interfaces";
+import * as React from "react";
+import { mount } from "enzyme";
+import { NavBar } from "../index";
+import { Everything } from "../../interfaces";
 
-// describe("<NavBar", function () {
-//     it("renders helpful text", function () {
-//         let props: Everything = {
-//             config: {
-//                 host: "",
-//                 port: ""
-//             },
-//             auth: {},
-//             designer: {},
-//             dispatch: jest.fn(),
-//             bot: {},
-//             sequences: {},
-//             regimens: {},
-//             bulkScheduler: {},
-//             location: {},
-//             draggable: {},
-//             peripherals: {},
-//             tools: {},
-//             sync: {}
-//         };
-//         let dom = mount(<NavBar {...props } />);
-//         // expect(dom.html()).toContain("Page Not Found");
-//     });
-// });
+describe("<NavBar", function () {
+    it("renders helpful text", function () {
+        pending("Work in progress...");
+        // let props: Everything = {
+        //     config: {
+        //         host: "",
+        //         port: ""
+        //     },
+        //     auth: {},
+        //     designer: {},
+        //     dispatch: jest.fn(),
+        //     bot: {},
+        //     sequences: {},
+        //     regimens: {},
+        //     bulkScheduler: {},
+        //     location: {},
+        //     draggable: {},
+        //     peripherals: {},
+        //     tools: {},
+        //     sync: {}
+        // };
+        // let dom = mount(<NavBar {...props } />);
+        // expect(dom.html()).toContain("Page Not Found");
+    });
+});
