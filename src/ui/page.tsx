e2907b312073c8b3f6de3c82e1dd9e49a883dd85
MrChristofferson <chrisvan0848@gmail.com>
2017-01-08 23:00:38 -0700

    more prepping for mobile app

--- src/ui/page.tsx
+++ src/ui/page.tsx
@@ -6,11 +6,9 @@ interface PageProps {
 }
 
 export function Page(props: PageProps) {
-    let finalClassName = "page container";
+    let finalClassName = "page";
     if (props.className) { finalClassName += ` ${props.className}`; }
     return <div className={finalClassName}>
-        <div className="row">
-            {props.children}
-        </div>
+        {props.children}
     </div>;
 }
