282159e8a05b1e79419eab73d5705cdbfa08e484
Rick Carlino <rick.carlino@gmail.com>
2017-01-26 10:11:40 -0600

    Remove unused typings/ NPM packages. Closes #174

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,5 +2,3 @@
 /// <reference path="axios.d.ts" />
 /// <reference path="i18n.d.ts" />
 /// <reference path="react-redux.d.ts" />
-/// <reference path="react-canvas.d.ts" />
-/// <reference path="snapsvg.d.ts" />
