f2c8e3a7ee4dc7fc4414e336af9fdf77728a2b40
Elender <elenderg@gmail.com>
2017-01-28 19:08:40 -0300

    Update pt-br.js

--- public/app-resources/languages/pt-br.js
+++ public/app-resources/languages/pt-br.js
@@ -1,4 +1,46 @@
 module.exports = {
   "Enter Password": "Digite sua Senha",
-  "Day {{day}}": "Dia {{day}}"
+  "Day {{day}}": "Dia {{day}}",
+  "User successfully updated." : "Usuário atualizado.",
+  "We're sorry to see you go. :(" : "É uma pena que você tenha de ir. :( ",
+  "Change Password" : "Alterar Senha",
+  "Old Password" : "Senha Anterior",
+  "New Password" : "Nova Senha",
+  "DELETE ACCOUNT" : "EXCLUIR A CONTA",
+  "SAVE" : "SALVAR",
+  "Your Name" : "Seu Nome",
+  "Email" : "E-mail",
+  "MOVE AMOUNT (mm)" : "DISTÃNCIA DO MOVIMENTO (mm)",
+  "Edit" : "Editar",
+  "Camera" ; "Câmera",
+  "Please enter a valid label and pin number." : "Por favor insira um marcador e um número PIN válidos.",
+  "Pin {{num}}" : "PIN - Número de Identificação Pessoal {{num}}",
+  "no" : "não",
+  "yes" : "sim",
+  
+  
+  // BELOW LINES ARE FOR TEST PURPOSES ONLY - DO NOT UNCOMMENT UNLESS YOU KNOW WHAT YOU ARE DOING!
+  //"Enter your password to delete your account." : "Digite sua senha pra excluir a conta.",
+  //"`User could not be updated: ${e.message}`" : "`Não foi possível atualizar o usuário: ${e.message}`",
+  //"Impossible" :"Impossível",
+  //"You have been logged out." "Você encerrou sua sessão.",
+  //`Use these manual control buttons to move FarmBot in realtime. Press the arrows for relative movements or type in new coordinates and press GO for an absolute movement.   Tip: Press the Home button when you are done so FarmBot is ready to get back to work.` : `Utilize estes botões de controle manual para movimentar o FarmBot em tempo real.  Pressione as setas para movimentar ou insira novas cordenadas e aperte o  botão IR para um movimento mais preciso. Dica: Aperte o botão de Início quando terminar para que o Farmbot saiba que deve voltar ao trabalho.`,
+  //`Press the edit button to update and save your webcam URL.` :   //`Pressione o botão de editar para atualizar e salvar a URL de sua webcam.`,
+  //`Use these toggle switches to control FarmBot's peripherals in realtime. To edit and create new peripherals, press the EDIT button. Make sure to turn things off when you're done!` : `Use esses controles para  controlar os periféricos do Farmbot em tempo real. Para editar e criar novos periféricos, pressione o botão EDITAR. Tenha certeza de desligar tudo quando acabar!`
+  //`Tried to connect to null bot.You probably meant to set a bot first.` : `Tentou conectar a um farmbot inexistente. Você provavelmente esqueceu de configurar seu farmbot.`
+  //
+  //
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 }
