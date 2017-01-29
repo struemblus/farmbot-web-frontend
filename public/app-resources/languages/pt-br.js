e008d3cfae0de80472f8ff749cd46d4543162c0c
Elender <elenderg@gmail.com>
2017-01-29 10:36:54 -0300

    Update pt-br.js

--- public/app-resources/languages/pt-br.js
+++ public/app-resources/languages/pt-br.js
@@ -15,7 +15,7 @@ module.exports = {
   "INVERT MOTORS" : "INVERTER MOTORES",
   "LENGTH (m)" : "COMPRIMENTO (m)",
   "MAX SPEED (mm/s)" : "VELOCIDADE MÁXIMA (mm/s)",
-  "MOVE AMOUNT (mm)" : "DISTÃNCIA DO MOVIMENTO (mm)",
+  "MOVE AMOUNT (mm)" : "DISTÂNCIA DO MOVIMENTO (mm)",
   "NAME" : "NOME";
   "NETWORK" : "REDE",
   "RESET" : "RESETAR TUDO",
@@ -65,7 +65,7 @@ module.exports = {
   "Could not download firmware update information." : "Não foi possível transferir as informações de atualização do Firmware do dispositivo.",
   "Could not download OS update information." : "Não foi possível transferir as informações de atualização do Sistema Operacional.",
   "Could not fetch bot status. Is FarmBot online?" : "Não foi possível verificar o estado do farmbot. Verifique se o mesmo se encontra conectado",
-  "Couldn\'t save device." ; "Não foi possível salvar dados sobre o o dispositivo",
+  "Couldn\'t save device." : "Não foi possível salvar dados sobre o o dispositivo",
   "downloading device credentials" : "transferindo credenciais do dispositivo",
   "Error establishing socket connection" : "Erro ao estabelecer conexão com os soquetes",
   "Error while saving device." : "Erro ao salvar detalhes do dispositivo.",
@@ -81,13 +81,15 @@ module.exports = {
   "Tried to delete plant, but couldn't." : "Não foi possível excluir esta planta.",
   "Tried to move plant, but couldn't." : "Não foi possível mover esta planta.",
   "Tried to save plant, but couldn't." : "Não foi possível salvar os dados desta planta.",
-  "Unable to download device credentials" ; "Não foi possível transferir as credenciais do dispositivo",
+  "Unable to download device credentials" : "Não foi possível transferir as credenciais do dispositivo",
   "User successfully updated." : "Usuário atualizado.",
   "You may need to upgrade FarmBot OS. " : "Talvez você tenha que aprimorar o o Sistema Operacional do Farmbot. ",
   
 
   //OTHERS/GENERAL WORDS
+  "Add" : "Adicionar",
   "Day {{day}}": "Dia {{day}}",
+  "Delete" : "Excluir",
   "Edit" : "Editar",
   "no" : "não",
   "Parameters" : "Parâmetros",
@@ -95,11 +97,13 @@ module.exports = {
   "Reset" : "Redefinir",  
   "Server Port" : "Porta do Servidor",
   "Server URL" : "URL do Servidor",
-  "Starts" ; "Inicia em",
-  "Steps per MM" ; "Passos por unidade MM",
+  "Starts" : "Inicia em",
+  "Steps per MM" : "Passos por unidade MM",
+  "Time" : "Tempo",
   "Until" : "Até",
   "Version" : "Versão",
-  "Weed Detector" ; "Detector de Ervas Daninhas",
+  "Weed Detector" : "Detector de Ervas Daninhas",
+  "Week" : "Semana",
   "We're sorry to see you go. :(" : "É uma pena que você tenha de ir. :( ",
   "yes" : "sim",
 
@@ -108,36 +112,56 @@ module.exports = {
   "Sequence or Regimen" : "Sequência ou Regime",
   "Regimen deleted." : "Programação excluída.",
   "Regimen saved." : "Programação Salva.",
+  "Select a regimen or create one first." : "Selecione uma programação. Você deve criar a sua, se ainda não tiver feito isso.",
   "Select a sequence from the dropdown first." : "Selecione uma sequência da lista primeiro.",
+  "This regimen is currently empty." : "Esta programação não possui nenhuma sequência agendada."
   "Unable to delete regimen." ; "Erro ao deletar programação.",
   "Unable to save regimen." : "A programação não pode ser salva.",
   
   
   
   // BELOW LINES ARE FOR TEST PURPOSES ONLY - DO NOT UNCOMMENT UNLESS YOU KNOW WHAT YOU ARE DOING!
+  
+  //ACCOUNT MESSAGES
   //"Enter your password to delete your account." : "Digite sua senha pra excluir a conta.",
   //`User could not be updated: ${e.message}` : "`Não foi possível atualizar o usuário: ${e.message}`,
-  //"Impossible" :"Impossível",
   //"You have been logged out." "Sessão encerrada.",
   
-  //`Use these manual control buttons to move FarmBot in realtime. Press the arrows for relative movements or type in new coordinates and press GO for an absolute movement.   Tip: Press the Home button when you are done so FarmBot is ready to get back to work.` : `Utilize estes botões de controle manual para movimentar o FarmBot em tempo real.  Pressione as setas para movimentar ou insira novas cordenadas e aperte o  botão IR para um movimento mais preciso. Dica: Aperte o botão de Início quando terminar para que o Farmbot saiba que deve voltar ao trabalho.`,
-  
-  //`Press the edit button to update and save your webcam URL.` :   //`Pressione o botão de editar para atualizar e salvar a URL de sua webcam.`,
- 
-  //`Use these toggle switches to control FarmBot's peripherals in realtime. To edit and create new peripherals, press the EDIT button. Make sure to turn things off when you're done!` : `Use esses controles para  controlar os periféricos do Farmbot em tempo real. Para editar e criar novos periféricos, pressione o botão EDITAR. Tenha certeza de desligar tudo quando acabar!`
-  
-  //`Tried to connect to null bot.You probably meant to set a bot first.` : `Tentou conectar a um farmbot inexistente. Você provavelmente esqueceu de configurar seu farmbot.`
-  
-  //"request sent to device." : " solicitação enviada ao dispositivo."
-  //"Setting toggle" : "Configuração de alternadores",
-  //"Relative movement" : "Movimento Simples"
+  //GENERAL TEXTS
   //"Absolute movement" : "Movimento Preciso"
+  //`Change settings` : `Alterar configurações`,
+  //`Detect Weeds` : `Detectar Ervas Daninhas`,
+  //"Impossible" :"Impossível",
+  //"Relative movement" : "Movimento Simples"
+  //"Setting toggle" : "Configuração de alternadores",
   //`This widget` : `este widget`,
+    
+  //TIPS & ERRORS
+  //`Use these manual control buttons to move FarmBot in realtime. Press the arrows for relative movements or type in new coordinates and press GO for an absolute movement.   Tip: Press the Home button when you are done so FarmBot is ready to get back to work.` : `Utilize estes botões de controle manual para movimentar o FarmBot em tempo real.  Pressione as setas para movimentar ou insira novas cordenadas e aperte o  botão IR para um movimento mais preciso. Dica: Aperte o botão de Início quando terminar para que o Farmbot saiba que deve voltar ao trabalho.`,
+  //`Press the edit button to update and save your webcam URL.` :  `Pressione o botão de editar para atualizar e salvar a URL de sua webcam.`,
+  //`Use these toggle switches to control FarmBot's peripherals in realtime. To edit and create new peripherals, press the EDIT button. Make sure to turn things off when you're done!` : `Use esses controles para  controlar os periféricos do Farmbot em tempo real. Para editar e criar novos periféricos, pressione o botão EDITAR. Tenha certeza de desligar tudo quando acabar!`
+  //`Tried to connect to null bot.You probably meant to set a bot first.` : `Tentou conectar a um farmbot inexistente. Você provavelmente esqueceu de configurar seu farmbot.`
+  //"request sent to device." : " solicitação enviada ao dispositivo."
   //`This will restart FarmBot's Raspberry` : `Isto irá reiniciar o equipamento Raspberry do FarmBot`,
   //`This will shutdown FarmBot's Raspberry Pi`. : `Isto irá desligar o  Raspberry Pi do FarmBot`.
-  //`Change settings` : `Alterar configurações`,
-  //`Detect Weeds` : `Detectar Ervas Daninhas`
+  
+  //REGIMEN & SEQUENCES
+  //`Use this tool to schedule sequences to run on one or many days of your regimen.` : `Use esta ferramenta para agendar sequências a serem executadas em um ou mais dias de sua programação.`,
+  //`You don't have any Regimens yet. Click "Add" from the Regimens widget to create and edit your first Regimen.` : `Você ainda não criou nenhuma programação. Clique em "Adicionar" a partir do widget de programações para criar e editar sua primeira programação.`,
+  //`Regimens allow FarmBot to take care of a plant throughout its entire life. A regimen consists of many sequences that are scheduled to run based on the age of the plant. Regimens are applied to plants from the farm designer (coming soon) and can be re-used on many plants growing at the same or different times. Multiple regimens can be applied to any one plant.` : `As programações permitem que o FarmBot cuide de uma planta ao longo de toda a vida dela. Uma programação consiste em várias sequências que tem sua execução agendada  de acordo com a idade da planta. As programações são aplicadas às plantas a partir do Planejador de hortas (disponível em breve) e podem ser reutilizadas  em várias plantas  de idades iguais ou diferentes. Várias programações podem ser atribuídas à uma mesma planta.`,
+  //`Add sequences to this Regimen by using the "scheduler"` : `Adicione Sequências à esta programação utilizando o "agendador"`,
+  
   //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  
+  
+  
   
 
   
