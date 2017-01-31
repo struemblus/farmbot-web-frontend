b548285f9d27a631ea2556d62b88772bbd403999
Rick Carlino <rick.carlino@gmail.com>
2017-01-31 09:56:12 -0600

    TOS API calls and forms work. Need to add conditional links to TOS.

--- src/tos_update/index.tsx
+++ src/tos_update/index.tsx
@@ -2,43 +2,64 @@ import * as React from "react";
 import { render } from "react-dom";
 import * as axios from "axios";
 import { t, init } from "i18next";
+import { AuthState } from "../auth/interfaces";
+import { Session } from "../session";
+import { error as log, init as logInit } from "../ui";
+import { prettyPrintApiErrors } from "../util";
 import "../npm_addons";
 import { detectLanguage } from "../i18n";
+import { API } from "../api";
+
 import "../css/_index.scss";
 import "../npm_addons";
 
 interface Props { };
-interface State { };
+interface State {
+    email: string;
+    password: string;
+    agree_to_terms: boolean;
+    serverHost: string;
+    serverPort: string;
+};
 
 
-export class Wow extends React.Component<Props, State> {
+export class Wow extends React.Component<Props, Partial<State>> {
     constructor() {
         super();
-        this.state = {};
+        this.submit = this.submit.bind(this);
+        this.state = {
+            serverHost: API.fetchHostName(),
+            serverPort: API.inferPort()
+        };
     }
 
-    componentDidMount() {
-        console.log("Component did mount.");
-    }
-
-    set(name: string) {
+    set(name: keyof State) {
         return function (event: React.FormEvent<HTMLInputElement>) {
-            let state: { [name: string]: string } = {};
+            let state: { [name: string]: State[keyof State] } = {};
             state[name] = (event.currentTarget).value;
             this.setState(state);
         };
     }
 
-    submit(e: React.SyntheticEvent<HTMLInputElement>) {
+    submit(e: React.SyntheticEvent<HTMLFormElement>) {
         e.preventDefault();
+        let { email, password, agree_to_terms } = this.state;
+        let payload = { user: { email, password, agree_to_terms } };
+        let url = `//${this.state.serverHost}:${this.state.serverPort}`;
+        API.setBaseUrl(url);
         axios
-            .put<{}>("/CHANGE_THIS", { FOO: "bar" })
+            .post<AuthState>(API.current.tokensPath, payload)
             .then(resp => {
-                window.location.href = "/";
+                Session.put(resp.data);
+                window.location.href = "/app/controls";
             })
             .catch(error => {
-                console.log("Unhandled error :(");
+                log(prettyPrintApiErrors(error));
             });
+    }
+
+    componentDidMount() {
+        logInit();
     }
 
     render() {
@@ -55,35 +76,41 @@ export class Wow extends React.Component<Props, State> {
                             </div>
                         </div>
                         <div className="row">
-                            <form onSubmit={this.submit.bind(this)}>
+                            <form onSubmit={this.submit}>
                                 <div className="col-sm-12">
                                     <div className="widget-content">
                                         <div className="input-group">
-                                            <label>{t("Email")}</label>
-                                            <input type="text"
-                                                onChange={this.set("email")
-                                                    .bind(this)}>
+                                            <label> {t("Email")} </label>
+                                            <input type="email"
+                                                onChange={this.set("email").bind(this)}>
                                             </input>
-                                            <label>
-                                                {t("Password")}
-                                            </label>
+                                            <label>{t("Password")}</label>
                                             <input type="password"
-                                                onChange={this.set(
-                                                    "password")
-                                                    .bind(this)}>
+                                                onChange={this.set("password").bind(this)}>
                                             </input>
                                             <label>
-                                                {t("I Agree")}
+                                                <input type="checkbox"
+                                                    onChange={this.set("agree_to_terms").bind(this)} />
+                                                {t("Accept")}
                                             </label>
-                                            <input type="checkbox"
-                                                onChange={() => { alert("TODO") }} />
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-xs-12">
-                                                <button className={`button-like 
-                                                    button green login`}>
-                                                    {t("Reset")}
-                                                </button>
+                                            <div className="row">
+                                                <div className="col-xs-12">
+                                                    <button className="button-like button green login">
+                                                        {t("I Agree to the Terms of Use")}
+                                                    </button>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <label>{t("Server URL")}</label>
+                                                <input type="text"
+                                                    onChange={this.set("serverHost").bind(this)}
+                                                    value={this.state.serverHost}>
+                                                </input>
+                                                <label>{t("Server Port")}</label>
+                                                <input type="text"
+                                                    onChange={this.set("serverPort").bind(this)}
+                                                    value={this.state.serverPort}>
+                                                </input>
                                             </div>
                                         </div>
                                     </div>
