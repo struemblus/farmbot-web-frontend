4843467484afeecb339444a82cd387bd59e8fae4
MrChristofferson <chrisvan0848@gmail.com>
2017-02-13 16:21:33 -0700

    mobile designer additions

--- src/farm_designer/plants/species_catalog.tsx
+++ src/farm_designer/plants/species_catalog.tsx
@@ -8,65 +8,63 @@ import { connect } from "react-redux";
 import { t } from "i18next";
 
 interface SpeciesCatalogTileProps {
-  result: CropLiveSearchResult;
-  dispatch: Function;
+    result: CropLiveSearchResult;
+    dispatch: Function;
 }
 
 interface SearchBoxParams {
-  query: string;
-  dispatch: Function;
+    query: string;
+    dispatch: Function;
 }
 
 function SearchBox({query, dispatch}: SearchBoxParams) {
-  return <input value={query}
-    onChange={(e) => doSearch(e, dispatch)}
-    className="search"
-    placeholder="Search OpenFarm" />;
+    return <input value={query}
+        onChange={(e) => doSearch(e, dispatch)}
+        className="search"
+        placeholder="Search OpenFarm" />;
 }
 
 function doSearch(e: React.FormEvent<HTMLInputElement>,
-  dispatch: Function) {
-  dispatch(openFarmSearchQuery(e.currentTarget.value));
+    dispatch: Function) {
+    dispatch(openFarmSearchQuery(e.currentTarget.value));
 }
 
 @connect((state: Everything) => state)
 export class SpeciesCatalog extends React.Component<Everything, {}> {
-  render() {
-    let species = this.props.designer.cropSearchResults.map((resp, index) => {
-      return <div className="plant-catalog-tile" key={index}>
-        <div className="small-header-wrapper">
-          <label>{resp.crop.name}</label>
-        </div>
-        <div>
-          <p>
-            <Link to={`/app/designer/plants/crop_search/${resp.crop.slug}`}>
-              <img className="crop-drag-info-image" src={resp.image} />
-            </Link>
-          </p>
-        </div>
-      </div>;
-    });
+    render() {
+        let { cropSearchResults, cropSearchQuery } = this.props.designer;
 
-    return <div className="panel-container green-panel">
-      <div className="panel-header green-panel">
-        <p className="panel-title">
-          <BackArrow /> {t("Choose a species")}
-        </p>
-      </div>
-      <div className="panel-content">
-        <div className="thin-search-wrapper">
-          <i className="fa fa-search"></i>
-          <div className="thin-search">
-            <SearchBox query={this.props.designer.cropSearchQuery}
-              dispatch={this.props.dispatch} />
-          </div>
-        </div>
-        <div className="panel-content">
-          <div className="crop-search-result-wrapper">
-            {species}
-          </div>
-        </div>
-      </div>
-    </div>;
-  }
+        let species = cropSearchResults.map((resp, index) => {
+            let { crop, image } = resp;
+            return <div className="plant-catalog-tile col-xs-6" key={index}>
+                <label>{crop.name}</label>
+                <Link
+                    to={`/app/designer/plants/crop_search/${crop.slug}`}>
+                    <img className="crop-drag-info-image" src={image} />
+                </Link>
+            </div>;
+        });
+
+        return <div className="panel-container green-panel">
+            <div className="panel-header green-panel">
+                <p className="panel-title">
+                    <BackArrow /> {t("Choose a species")}
+                </p>
+            </div>
+            <div className="panel-content">
+                <div className="thin-search-wrapper">
+                    <i className="fa fa-search"></i>
+                    <div className="thin-search">
+                        <SearchBox query={cropSearchQuery}
+                            dispatch={this.props.dispatch} />
+                    </div>
+                </div>
+                <div className="panel-content">
+                    <div className="crop-search-result-wrapper row">
+                        {species}
+                    </div>
+                </div>
+            </div>
+        </div>;
+    }
 }
