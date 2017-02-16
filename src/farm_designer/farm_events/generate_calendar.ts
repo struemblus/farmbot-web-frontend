05f5ef3d150eeeccdb59fa8acc0f118e348ffd24
Rick Carlino <rick.carlino@gmail.com>
2017-02-16 11:30:49 -0600

    Remove later.js

--- src/farm_designer/farm_events/generate_calendar.ts
+++ src/farm_designer/farm_events/generate_calendar.ts
@@ -25,27 +25,10 @@ function putEventIntoCalendarMap(target: DateLookup,
 
 export function generateCalendar(input: FarmEvent[]) {
   let calendar: DateLookup = {};
-
-  let rules = input.map(function (fe) {
+  input.map(function (fe) {
     if (fe.time_unit === "never") {
       putEventIntoCalendarMap(calendar, fe.start_time, fe);
-      return fe.start_time;
     } else {
-      let { start_time, end_time, repeat, time_unit } = fe;
-      let r = wow.parse.recur();
-      r = end_time ? r.between(start_time, end_time) : r.startingOn(start_time);
-      r = r.every(repeat);
-      debugger;
-      switch (time_unit) {
-        case "minutely": return r.minute();
-        case "hourly": return r.hour();
-        case "daily": return r.dayOfYear();
-        case "weekly": return r.weekOfYear();
-        case "monthly": return r.month();
-        case "yearly": return r.year();
-        default: throw new Error(`${time_unit} is not an expected time_unit`);
-      }
     }
   });
-  return rules;
 }
