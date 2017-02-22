40d1f53ce3574ebd702afdacb8182621dcc94a1e
MrChristofferson <chrisvan0848@gmail.com>
2017-02-21 17:10:21 -0700

    css

--- src/regimens/bulk_scheduler/index.tsx
+++ src/regimens/bulk_scheduler/index.tsx
@@ -9,49 +9,47 @@ import { WeekGrid } from "./week_grid";
 import { commitBulkEditor } from "./actions";
 import { t } from "i18next";
 interface BulkEditorProps {
-    sequences: Sequence[];
-    editor: BulkSchedulerState;
-    dispatch: Function;
+  sequences: Sequence[];
+  editor: BulkSchedulerState;
+  dispatch: Function;
 }
 
 export function BulkSchedulerWidget({sequences, dispatch, editor}:
-    BulkEditorProps) {
-    let click = function () { dispatch(commitBulkEditor()); };
-    let active = !!(sequences && sequences.length);
-    return (<div>
-        <div className="widget-wrapper bulk-scheduler-widget">
-            <div className="row">
-                <div className="col-sm-12">
-                    <AddButton active={active} click={click} />
-                    <div className="widget-header">
-                        <h5>Scheduler</h5>
-                        <i className="fa fa-question-circle widget-help-icon">
-                            <div className="widget-help-text">{t(`Use this tool
+  BulkEditorProps) {
+  let click = function () { dispatch(commitBulkEditor()); };
+  let active = !!(sequences && sequences.length);
+  return <div className="widget-wrapper bulk-scheduler-widget">
+    <div className="row">
+      <div className="col-sm-12">
+        <AddButton active={active} click={click} />
+        <div className="widget-header">
+          <h5>Scheduler</h5>
+          <i className="fa fa-question-circle widget-help-icon">
+            <div className="widget-help-text">{t(`Use this tool
                                 to schedule sequences to run on one or many
                                 days of your regimen.`)}</div>
-                        </i>
-                    </div>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-sm-12">
-                    <div className="widget-content">
-                        <div className="row">
-                            <div className="col-sm-6">
-                                <SequenceList sequences={sequences}
-                                    current={editor.sequence || nullSequence()}
-                                    dispatch={dispatch} />
-                            </div>
-                            <div className="col-sm-6">
-                                <TimeInput dispatch={dispatch}
-                                    offset={editor.form.dailyOffsetMs} />
-                            </div>
-                        </div>
-                        <WeekGrid weeks={editor.form.weeks}
-                            dispatch={dispatch} />
-                    </div>
-                </div>
-            </div>
+          </i>
         </div>
-    </div>);
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-sm-12">
+        <div className="widget-content">
+          <div className="row">
+            <div className="col-sm-6">
+              <SequenceList sequences={sequences}
+                current={editor.sequence || nullSequence()}
+                dispatch={dispatch} />
+            </div>
+            <div className="col-sm-6">
+              <TimeInput dispatch={dispatch}
+                offset={editor.form.dailyOffsetMs} />
+            </div>
+          </div>
+          <WeekGrid weeks={editor.form.weeks}
+            dispatch={dispatch} />
+        </div>
+      </div>
+    </div>
+  </div>;
 }
--- src/regimens/index.tsx
+++ src/regimens/index.tsx
@@ -13,23 +13,21 @@ export class Regimens extends React.Component<RegimenPropsWithParams, {}> {
   render() {
     let { bulkScheduler } = this.props;
 
-    return <div>
-      <div className="all-content-wrapper">
-        <div className="row">
-          <div className={`col-md-3 col-sm-12 col-md-offset-1`}>
-            <BulkSchedulerWidget editor={bulkScheduler}
-              sequences={this.props.sequences.all}
-              dispatch={this.props.dispatch} />
-          </div>
-          <div className="col-md-4 col-sm-12">
-            <RegimenEditorWidget { ...this.props } />
-          </div>
-          {isMobile() && (
-            <MobileRegimensNav { ...this.props} />
-          )}
-          <div className="col-md-3 col-sm-12">
-            <RegimensList { ...this.props } />
-          </div>
+    return <div className="all-content-wrapper">
+      <div className="row">
+        <div className="col-md-4 col-sm-12">
+          <BulkSchedulerWidget editor={bulkScheduler}
+            sequences={this.props.sequences.all}
+            dispatch={this.props.dispatch} />
+        </div>
+        <div className="col-md-4 col-sm-12">
+          <RegimenEditorWidget { ...this.props } />
+        </div>
+        {isMobile() && (
+          <MobileRegimensNav { ...this.props} />
+        )}
+        <div className="col-md-4 col-sm-12">
+          <RegimensList { ...this.props } />
         </div>
       </div>
     </div>;
