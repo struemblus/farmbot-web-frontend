19c13514cd38b770799e72fc39a6dca300f50cc1
Rick Carlino <rick.carlino@gmail.com>
2017-03-03 10:33:51 -0600

    Bump farmbotJS + syncStatus texts.'

---
+++ src/nav/sync_button.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { SyncStatus } from "farmbot/dist";
+import { NavButtonProps } from "./interfaces";
+import { sync } from "../devices/actions";
+
+const COLOR_MAPPING: Partial<Record<SyncStatus, string>> = {
+  "synced": "green",
+  "sync_now": "yellow",
+  "syncing": "yellow",
+  "sync_error": "red",
+  "unknown": "red"
+};
+
+const TEXT_MAPPING: Partial<Record<SyncStatus, string>> = {
+  "synced": "SYNCED",
+  "sync_now": "SYNC NOW",
+  "syncing": "SYNCING",
+  "sync_error": "SYNC ERROR",
+  "unknown": "OFFLINE"
+};
+
+export function SyncButton({ auth, bot, dispatch }: NavButtonProps) {
+  if (!auth) {
+    return <span></span>;
+  }
+  let { sync_status } = bot.hardware.informational_settings;
+  sync_status = sync_status || "unknown";
+  let color = COLOR_MAPPING[sync_status] || "red";
+  let text = TEXT_MAPPING[sync_status] || "offline";
+  return <button className={`nav-sync button-like ${color}`}
+    onClick={() => { dispatch(sync()); }}>
+    {text}
+  </button>;
+};
