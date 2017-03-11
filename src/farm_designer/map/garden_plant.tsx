9fa64bdf0c0f9e5e67bb810f36392939c4daa031
MrChristofferson <chrisvan0848@gmail.com>
2017-03-11 10:13:25 -0700

    increase plant pixel width

--- src/farm_designer/map/garden_plant.tsx
+++ src/farm_designer/map/garden_plant.tsx
@@ -3,23 +3,23 @@ import { DraggableSvgImage } from "../draggable_svg_image";
 import { Plant } from "../interfaces";
 
 interface Props {
-    plant: Plant;
-    onUpdate: (deltaX: number, deltaY: number, idx: number) => void;
-    onDrop: (id: number) => void;
+  plant: Plant;
+  onUpdate: (deltaX: number, deltaY: number, idx: number) => void;
+  onDrop: (id: number) => void;
 }
 export function GardenPlant(props: Props) {
-    let { plant, onUpdate, onDrop } = props;
-    if (plant.id) {
-        return <DraggableSvgImage key={plant.id}
-            x={plant.x}
-            y={plant.y}
-            height={32}
-            width={32}
-            id={plant.id}
-            onUpdate={onUpdate}
-            onDrop={onDrop}
-            href={"/app-resources/img/icons/Sprout-96.png"} />;
-    } else {
-        throw new Error("Save plants before placing them on the map");
-    }
+  let { plant, onUpdate, onDrop } = props;
+  if (plant.id) {
+    return <DraggableSvgImage key={plant.id}
+      x={plant.x}
+      y={plant.y}
+      height={50}
+      width={50}
+      id={plant.id}
+      onUpdate={onUpdate}
+      onDrop={onDrop}
+      href={"/app-resources/img/icons/Sprout-96.png"} />;
+  } else {
+    throw new Error("Save plants before placing them on the map");
+  }
 }
