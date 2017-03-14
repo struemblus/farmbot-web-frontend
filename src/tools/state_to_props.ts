f5a4ef0d9dec09558e0cdbf09ed7a2674466cf10
Rick Carlino <rick.carlino@gmail.com>
2017-03-14 13:48:39 -0500

    [STABLE] :basketball:

--- src/tools/state_to_props.ts
+++ src/tools/state_to_props.ts
@@ -1,6 +1,7 @@
 import { Everything } from "../interfaces";
 import { Props } from "./interfaces";
 import * as _ from "lodash";
+import { never } from "../util";
 
 export function mapStateToProps(props: Everything): Props {
   let toolBays = props.tools.tool_bays;
@@ -23,22 +24,14 @@ export function mapStateToProps(props: Everything): Props {
 
   /** Returns all tools in an <FBSelect /> compatible format. */
   let getToolOptions = () => {
-    return tools.map(tool => {
-      return { label: tool.name, value: tool.id };
-    })
-      .filter(ddi => !!ddi.value)
-      .map(ddi => {
-        if (ddi.value) {
-          return ddi;
-        } else {
-          // Being extra safe for ToolBay form being out of hand.
-          // If no errors, remove after April 15, 2017.
-          throw new Error(".filter failed to filter dropdown item.");
-        }
-      });
+    return _(tools)
+      .map(tool => ({ label: tool.name, value: (tool.id as number) }))
+      .filter(ddi => _.isNumber(ddi.value))
+      .compact()
+      .value();
   };
 
-	/** Returns the current tool chosen in a slot based off the slot's id 
+	/** Returns the current tool chosen in a slot based off the slot's id
 	 * and in an <FBSelect /> compatible format. */
   let getChosenToolOption = (toolSlotId: number) => {
     let currentSlot = _.findWhere(toolSlots, { id: toolSlotId });
