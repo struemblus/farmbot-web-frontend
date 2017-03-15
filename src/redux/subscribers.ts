9ee82338dfe094b5ad320bb14422023d9d919c53
MrChristofferson <chrisvan0848@gmail.com>
2017-03-14 21:52:25 -0700

    refactor /redux

--- src/redux/subscribers.ts
+++ src/redux/subscribers.ts
@@ -4,30 +4,30 @@ import { dontExitIfBrowserIsOnHold } from "../browser_holds/index";
 import { EnvName } from "./interfaces";
 
 interface Subscription {
-    fn: (state: Everything) => void;
-    env: EnvName;
+  fn: (state: Everything) => void;
+  env: EnvName;
 };
 
 /** To make it easier to manage all things watching the state tree,
  * we keep subscriber functions in this array. */
 export let subscriptions: Subscription[] = [
-    {
-        env: "development",
-        fn: function storeState(state: Everything) {
-            sessionStorage["lastState"] = JSON.stringify(state);
-        }
+  {
+    env: "development",
+    fn: function storeState(state: Everything) {
+      sessionStorage["lastState"] = JSON.stringify(state);
     }
-    , {
-        env: "*",
-        fn: dontExitIfBrowserIsOnHold
-    }
+  }
+  , {
+    env: "*",
+    fn: dontExitIfBrowserIsOnHold
+  }
 ];
 
 export function registerSubscribers(store: Store) {
-    let ENV_LIST = [process.env.NODE_ENV, "*"];
-    subscriptions.forEach(function(s) {
-        if (ENV_LIST.includes(s.env)) {
-            store.subscribe(() => s.fn(store.getState()));
-        };
-    });
+  let ENV_LIST = [process.env.NODE_ENV, "*"];
+  subscriptions.forEach(function (s) {
+    if (ENV_LIST.includes(s.env)) {
+      store.subscribe(() => s.fn(store.getState()));
+    };
+  });
 };
