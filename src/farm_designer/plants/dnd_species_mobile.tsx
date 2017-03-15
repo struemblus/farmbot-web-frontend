ae5e9ca707f0aaa8e5ddebcdd60159924a944d70
Rick Carlino <rick.carlino@gmail.com>
2017-03-15 17:00:15 -0500

    Better handling of browsers without DnD

--- src/farm_designer/plants/dnd_species_mobile.tsx
+++ src/farm_designer/plants/dnd_species_mobile.tsx
@@ -11,6 +11,10 @@ import {
   DraggableEvent
 } from "../interfaces";
 
+const NOOP = (a: object, b: number, c: number) => {
+  console.log("NO DND SUPPORT ON THIS BROWSER");
+};
+
 @connect((state: Everything) => state)
 /** DND => "drag and drop" */
 export class DNDSpeciesMobile extends React.Component<SpeciesInfoProps,
@@ -26,12 +30,8 @@ DNDSpeciesMobileState> {
     // Stub until we figure out dynamic drag images
     img.src = DEFAULT_ICON;
 
-    e
-      && e.dataTransfer
-      // Because of Android
-      && e.dataTransfer.setDragImage
-      // Because of MS Edge.
-      && e.dataTransfer.setDragImage(img, 50, 50);
+    // Because of Android and MS Edge.
+    _.get(e, "dataTransfer.setDragImage", NOOP)(img, 50, 50);
   }
 
   toggleDesignerView() {
--- src/images/weed_detector.tsx
+++ src/images/weed_detector.tsx
@@ -5,6 +5,8 @@ import { DetectorState } from "./interfaces";
 import { WeedDetectorBody } from "./weed_detector_body";
 import { TitleBar } from "./weed_detector_title";
 import { devices } from "../device";
+import { success } from "../ui/index";
+import { t } from "i18next";
 
 @connect((state: Everything) => state)
 export class WeedDetector extends React.Component<Everything, Partial<DetectorState>> {
@@ -13,20 +15,22 @@ export class WeedDetector extends React.Component<Everything, Partial<DetectorSt
     this.state = {};
   }
 
+  takePhoto = () => {
+    // TODO: Real handler / toast.
+    devices
+      .current
+      .takePhoto()
+      .then(() => success(t("Processing photo.")), () => { alert("Error taking photo"); });
+  }
+
   render() {
     let TODO_OPEN = false;
     return <div>
       <div className="widget-wrapper weed-detector-widget">
         <div className="row">
           <div className="col-sm-12">
-            <TitleBar onDeletionClick={() => { console.log("NOT FINISHED") }}
-              onPhotoClick={() => {
-                // TODO: Real handler / toast.
-                devices
-                  .current
-                  .takePhoto()
-                  .then(() => { }, () => { alert("Error taking photo"); });
-              }}
+            <TitleBar onDeletionClick={() => { console.log("NOT FINISHED"); }}
+              onPhotoClick={this.takePhoto}
               onSave={() => { console.log("NOT FINISHED"); }}
               onSettingToggle={() => { console.log("NOT FINISHED"); }}
               onTest={() => { console.log("NOT FINISHED"); }}
@@ -34,8 +38,7 @@ export class WeedDetector extends React.Component<Everything, Partial<DetectorSt
             <div className="row">
               <div className="col-sm-12">
                 <WeedDetectorBody images={this.props.sync.images}
-                  onSliderChange={() => {
-                  }}
+                  onSliderChange={() => { }}
                   H={3}
                   S={3}
                   V={3} />
