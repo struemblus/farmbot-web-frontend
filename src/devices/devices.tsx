8caa708b0f4ccb556824c5879fd1988dce05f316
MrChristofferson <chrisvan0848@gmail.com>
2017-03-14 19:42:07 -0700

    refactor farmware widget

--- src/devices/devices.tsx
+++ src/devices/devices.tsx
@@ -5,30 +5,23 @@ import { WeedDetector } from "../images";
 import { HardwareSettings } from "./components/hardware_settings";
 import { FarmbotOsSettings } from "./components/farmbot_os_settings";
 import { Farmware } from "../farmware";
+import { Page, Col } from "../ui/index";
 
 @connect((state: Everything) => state)
 export class Devices extends React.Component<Everything, {}> {
   render() {
     if (this.props.auth) {
       let auth = this.props.auth;
-      return <div className="all-content-wrapper">
-        <div className="row">
-          <div className="col-md-6 col-sm-6 col-xs-12 col-sm-12">
-            <div className="widget-wrapper farmware-widget">
-              <div className="row">
-                <Farmware bot={this.props.bot} />
-              </div>
-            </div>
-            <div className="row">
-              <FarmbotOsSettings {...this.props} auth={auth} />
-            </div>
-          </div>
-          <div className="col-md-6 col-sm-6 col-xs-12">
-            <WeedDetector {...this.props} />
-            <HardwareSettings {...this.props} />
-          </div>
-        </div>
-      </div>;
+      return <Page className="devices">
+        <Col xs={12} sm={6}>
+          <Farmware bot={this.props.bot} />
+          <FarmbotOsSettings {...this.props} auth={auth} />
+        </Col>
+        <Col xs={12} sm={6}>
+          <WeedDetector {...this.props} />
+          <HardwareSettings {...this.props} />
+        </Col>
+      </Page>;
     } else {
       throw new Error("Log in first");
     }
