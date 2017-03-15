c2ff2ddd1dfd84954639925a632033d8ea3ac225
Rick Carlino <rick.carlino@gmail.com>
2017-03-15 08:40:44 -0500

    Factor config into its own file

--- src/images/weed_detector.tsx
+++ src/images/weed_detector.tsx
@@ -13,6 +13,7 @@ import { success, error, FBSelect, Col, Row, DropDownItem } from "../ui";
 import { resetWeedDetection } from "./actions";
 import { weedDetectorENVsafeFetch } from "./weed_detector_env";
 import { Progress } from "../util";
+import { additionalSettingsMenu } from "./weed_detector_config";
 
 const DETECTOR_ENV = "PLANT_DETECTION_options";
 const LAST_CLIENT_CONNECTED = "LAST_CLIENT_CONNECTED";
@@ -128,75 +129,6 @@ export class WeedDetector extends React.Component<Everything, Partial<DetectorSt
     this.setState({ settingsMenuOpen: !this.state.settingsMenuOpen });
   }
 
-  additionalSettingsMenu = () => {
-    let calibrationAxes: DropDownItem[] = [
-      { label: "X", value: "x" }, { label: "Y", value: "y" }
-    ];
-    let originLocations: DropDownItem[] = [
-      { label: "Top Left", value: "top_left" },
-      { label: "Top Right", value: "top_right" },
-      { label: "Bottom Left", value: "bottom_left" },
-      { label: "Bottom Right", value: "bottom_right" }
-    ];
-    return <div className="additional-settings-menu"
-      onClick={(e) => e.stopPropagation()}>
-      {/* This menu needs to be nested in the <i> for css purposes. However,
-        * we do not want events in here to bubble up to the toggle method. */}
-      <label htmlFor="invert_hue_selection">
-        {t(`Invert Hue Range Selection`)}
-      </label>
-      <input type="checkbox" id="invert_hue_selection" />
-      <label htmlFor="calibration_object_separation">
-        {t(`Calibration Object Separation`)}
-      </label>
-      <input type="number" id="calibration_object_separation"
-        placeholder="(Number)" />
-      <label htmlFor="calibration_object_separation_axis">
-        {t(`Calibration Object Separation along axis`)}
-      </label>
-      <FBSelect
-        list={calibrationAxes}
-        placeholder="Select..."
-        id="calibration_object_separation_axis" />
-      <Row>
-        <Col xs={6}>
-          <label htmlFor="camera_offset_x">
-            {t(`Camera Offset X`)}
-          </label>
-          <input type="number" id="camera_offset_x" placeholder="(Number)" />
-        </Col>
-        <Col xs={6}>
-          <label htmlFor="camera_offset_y">
-            {t(`Camera Offset Y`)}
-          </label>
-          <input type="number" id="camera_offset_y" placeholder="(Number)" />
-        </Col>
-      </Row>
-      <label htmlFor="image_bot_origin_location">
-        {t(`Origin Location in Image`)}
-      </label>
-      <FBSelect
-        list={originLocations}
-        placeholder="Select..."
-        id="image_bot_origin_location" />
-      <Row>
-        <Col xs={6}>
-          <label htmlFor="coord_scale">
-            {t(`Pixel coordinate scale`)}
-          </label>
-          <input type="number" id="coord_scale"
-            placeholder="(Number)" step={0.10} />
-        </Col>
-        <Col xs={6}>
-          <label htmlFor="total_rotation_angle">
-            {t(`Camera rotation`)}
-          </label>
-          <input type="number" id="total_rotation_angle" placeholder="(Number)" />
-        </Col>
-      </Row>
-    </div>;
-  };
-
   render() {
     let H = (this.state.H || [0, 0]);
     let S = (this.state.S || [0, 0]);
@@ -230,7 +162,7 @@ export class WeedDetector extends React.Component<Everything, Partial<DetectorSt
                 {t("Calibrate")}
               </button>
               <i className="fa fa-cog" onClick={this.toggleSettingsMenu}>
-                {this.state.settingsMenuOpen && this.additionalSettingsMenu()}
+                {this.state.settingsMenuOpen && additionalSettingsMenu()}
               </i>
               <h5>{t("Weed Detector")}</h5>
               <i className={`fa fa-question-circle
