de788a7a69620c94716846672e38f5a605e75da4
MrChristofferson <chrisvan0848@gmail.com>
2017-03-14 20:59:10 -0700

    refactor draggable

--- src/draggable/drop_area.tsx
+++ src/draggable/drop_area.tsx
@@ -3,54 +3,52 @@ import * as React from "react";
 
 /** Style rules for the drop area when it is not dragged over. */
 const INVISIBLE_STYLE = {
-    width: "100%",
-    height: "3px",
-    zIndex: 10,
-    position: "relative"
+  width: "100%",
+  height: "3px",
+  zIndex: 10,
+  position: "relative"
 };
 
 export class DropArea extends React.Component<DropAreaProps, DropAreaState> {
-    constructor() {
-        super();
-        this.state = {
-            isHovered: false
-        };
-    }
+  constructor() {
+    super();
+    this.state = { isHovered: false };
+  }
 
-    dragOver(event: React.DragEvent<HTMLElement>) {
-        event.preventDefault();
-    }
+  dragOver = (event: React.DragEvent<HTMLElement>) => {
+    event.preventDefault();
+  }
 
-    drop(event: React.DragEvent<HTMLElement>) {
-        event.preventDefault();
-        let key = event.dataTransfer.getData("text");
-        let fn = this.props.callback;
-        if (fn) { fn(key); }
-        this.toggle();
-    }
+  drop = (event: React.DragEvent<HTMLElement>) => {
+    event.preventDefault();
+    let key = event.dataTransfer.getData("text");
+    let fn = this.props.callback;
+    if (fn) { fn(key); }
+    this.toggle();
+  }
 
-    toggle() {
-        this.setState({ isHovered: !this.state.isHovered });
-    }
+  toggle = () => {
+    this.setState({ isHovered: !this.state.isHovered });
+  }
 
-    visible() {
-        return <div className="drag-drop-area padding"
-            onDragOver={this.dragOver.bind(this)}
-            onDrop={this.drop.bind(this)}>
-            {this.props.children}
-        </div>;
-    }
+  visible() {
+    return <div className="drag-drop-area padding"
+      onDragOver={this.dragOver}
+      onDrop={this.drop}>
+      {this.props.children}
+    </div>;
+  }
 
-    hidden() {
-        return <div style={INVISIBLE_STYLE}></div>;
-    }
+  hidden() {
+    return <div style={INVISIBLE_STYLE}></div>;
+  }
 
-    render() {
-        let isVisible = this.props.isLocked || this.state.isHovered;
+  render() {
+    let isVisible = this.props.isLocked || this.state.isHovered;
 
-        return <div onDragLeave={this.toggle.bind(this)}
-            onDragEnter={this.toggle.bind(this)}>
-            {isVisible ? this.visible() : this.hidden()}
-        </div>;
-    }
+    return <div onDragLeave={this.toggle}
+      onDragEnter={this.toggle}>
+      {isVisible ? this.visible() : this.hidden()}
+    </div>;
+  }
 }
