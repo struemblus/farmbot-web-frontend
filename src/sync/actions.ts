8a51405357c527f8c91d97a916c26c0d9b02247a
MrChristofferson <chrisvan0848@gmail.com>
2017-03-16 15:02:44 -0700

    hotfix

--- src/sync/actions.ts
+++ src/sync/actions.ts
@@ -4,29 +4,66 @@ import { error } from "../ui";
 import { API } from "../api";
 import { t } from "i18next";
 import * as axios from "axios";
+import { Tool } from "../tools/interfaces";
+import { Sequence } from "../sequences/interfaces";
 
-export function fetchSyncData(): Thunk {
-  return (dispatch: Function, getState: Function) => {
-    axios.get<Sync>(API.current.syncPath)
-      .then(({ data }) => {
-        dispatch(fetchSyncDataOk(data));
-      }, (e: Error) => {
-        error(t("Could not download sync data"));
-        dispatch(fetchSyncDataNo(e));
-      });
-  };
+function getTools() {
+  return axios.get<Tool[]>(API.current.toolsPath)
+    .then((resp): Tool[] => {
+      return resp.data;
+      // dispatch(fetchSyncDataOk(data));
+    }, (e: Error) => {
+      error(t("Could not download sync data"));
+      // dispatch(fetchSyncDataNo(e));
+    });
 };
 
-function fetchSyncDataOk(sync: Sync) {
-  return {
-    type: "FETCH_SYNC_OK",
-    payload: sync
-  };
+function getSequences() {
+  return axios.get<Sequence[]>(API.current.sequencesPath)
+    .then((resp): Sequence[] => {
+      return resp.data;
+      // dispatch(fetchSyncDataOk(data));
+    }, (e: Error) => {
+      error(t("Could not download sync data"));
+      // dispatch(fetchSyncDataNo(e));
+    });
+};
+
+type Stub = [Tool[], Sequence[]];
+
+type ResponseTypes = Tool[]
+
+export function fetchSyncData() {
+  Promise.all<Stub>([getTools(), getSequences()]).then((data: any) => {
+  // let [tools, tools ] = data.map(r => r.data)
+});
 }
 
-function fetchSyncDataNo(err: Error) {
-  return {
-    type: "FETCH_SYNC_NO",
-    payload: {}
-  };
-}
+
+// _____________________________________ sync only
+
+// export function fetchSyncData(): Thunk {
+//   return (dispatch: Function, getState: Function) => {
+//     axios.get<Sync>(API.current.syncPath)
+//       .then(({ data }) => {
+//         dispatch(fetchSyncDataOk(data));
+//       }, (e: Error) => {
+//         error(t("Could not download sync data"));
+//         dispatch(fetchSyncDataNo(e));
+//       });
+//   };
+// };
+
+// function fetchSyncDataOk(sync: Sync) {
+//   return {
+//     type: "FETCH_SYNC_OK",
+//     payload: sync
+//   };
+// }
+
+// function fetchSyncDataNo(err: Error) {
+//   return {
+//     type: "FETCH_SYNC_NO",
+//     payload: {}
+//   };
+// }
