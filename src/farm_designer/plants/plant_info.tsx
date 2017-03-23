1679a3af9dc4b05bfe44455e4ad0152d9471c00b
MrChristofferson <chrisvan0848@gmail.com>
2017-03-23 11:39:12 -0700

    farm designer additions

--- src/farm_designer/plants/plant_info.tsx
+++ src/farm_designer/plants/plant_info.tsx
@@ -1,53 +1,63 @@
 import * as React from "react";
-import { Link } from "react-router";
-import { Everything } from "../../interfaces";
+import { BackArrow, error } from "../../ui";
 import { connect } from "react-redux";
 import * as moment from "moment";
 import { t } from "i18next";
-import { PlantInfoProps, Plant } from "../interfaces";
-import { error } from "../../ui/index";
+import { EditPlantInfoProps, Plant, PlantData } from "../interfaces";
 import { history } from "../../history";
+import { Everything } from "../../interfaces";
 import { findWhere } from "../../resources/selectors";
 import { isTaggedPlant } from "../../resources/tagged_resources";
+import { destroy } from "../../api/crud";
+import { Link } from "react-router";
 
-function mapStateToProps(props: Everything) {
-  let findCurrentPlant = (plant_id: number) => {
-    let query: Partial<Plant> = { id: plant_id };
-    let currentPlant = findWhere(props.resources.index, query);
-    return currentPlant;
+function mapStateToProps(props: Everything): EditPlantInfoProps {
+  // TODO: Handle this better with query params
+  let plant_id = parseInt(history.getCurrentLocation().pathname.split("/")[4]);
+  let query: Partial<Plant> = { id: plant_id };
+  let currentPlant = findWhere(props.resources.index, query);
+
+  let plant_info: undefined | PlantData = undefined;
+  if (currentPlant && currentPlant.body.id && isTaggedPlant(currentPlant)) {
+    let { name, x, y, planted_at, id } = currentPlant.body;
+    plant_info = { name, x, y, planted_at, id, uuid: currentPlant.uuid };
+  }
+
+  if (!plant_info) {
+    history.push("/app/designer/plants");
+    error("Plant could not be found.", "Error");
   }
 
   return {
-    // TODO: This is definitely not right, figure out query objects
-    plant_id: parseInt(history.getCurrentLocation().pathname.split("/")[4]),
-    findCurrentPlant
+    plant_info,
+    push: history.push,
+    dispatch: props.dispatch,
   }
 }
 
 @connect(mapStateToProps)
-export class PlantInfo extends React.Component<PlantInfoProps, {}> {
-  render() {
-    if (!isTaggedPlant(this.props.findCurrentPlant(this.props.plant_id))) {
-      history.push("/app/designer/plants");
-      error("Couldn't find plant.", "Error");
-      throw new Error(`VERY BAD!! Plant could not be found by id, 
-        possible stale data!`);
-    }
+export class PlantInfo extends React.Component<EditPlantInfoProps, {}> {
+  destroy = (plantUUID: string) => {
+    this.props.dispatch(destroy(plantUUID))
+      .then(() => history.push("/app/designer/plants"))
+      .catch(() => error("Could not delete plant.", "Error"))
+  }
 
-    let { name, x, y, planted_at, id } = this.props
-      .findCurrentPlant(this.props.plant_id).body;
+  fallback = () => {
+    return <span>Redirecting...</span>
+  }
 
+  default = (plant_info: PlantData) => {
+    let { planted_at, name, x, y, id } = plant_info;
     let dayPlanted = moment();
-    // Same day = 1 !0
     let daysOld = dayPlanted.diff(moment(planted_at), "days") + 1;
     let plantedAt = moment(planted_at).format("MMMM Do YYYY, h:mma");
-    return <div className="panel-container green-panel">
+
+    return <div className="panel-container green-panel" >
       <div className="panel-header green-panel">
         <p className="panel-title">
-          <Link to={`/app/designer/plants`} className="back-arrow">
-            <i className="fa fa-arrow-left"></i>
-          </Link>
-          <span className="title">{name}</span>
+          <BackArrow />
+          <span className="title">{t("Edit")} {name}</span>
           <Link to={`/app/designer/plants/` + (id || "BROKEN")
             .toString() + `/edit`}
             className="right-button">
@@ -67,6 +77,11 @@ export class PlantInfo extends React.Component<PlantInfoProps, {}> {
           <li>Soil Acidifier</li>
         </ul>
       </div>
-    </div>;
+    </div >;
+  }
+
+  render() {
+    let { plant_info } = this.props;
+    return plant_info ? this.default(plant_info) : this.fallback();
   }
 }
