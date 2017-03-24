cfbe4ec6a593fdbee8a6cc45e1db7c07b71852fd
MrChristofferson <chrisvan0848@gmail.com>
2017-03-24 10:26:39 -0700

    complete farm event crud

--- src/farm_designer/farm_events/edit_farm_event.tsx
+++ src/farm_designer/farm_events/edit_farm_event.tsx
@@ -6,83 +6,123 @@ import {
   BlurableInput,
   Col,
   Row,
-  BackArrow
+  BackArrow,
+  DropDownItem,
+  success
 } from "../../ui";
 import * as moment from "moment";
 import { connect } from "react-redux";
-import { mapStateToPropsAddEdit, } from "./map_state_to_props_add_edit";
+import { mapStateToPropsAddEdit } from "./map_state_to_props_add_edit";
+import { hasKey } from "../../util";
 import { history } from "../../history";
+import { TaggedFarmEvent } from "../../resources/tagged_resources";
+import { edit, save, destroy } from "../../api/crud";
 
 @connect(mapStateToPropsAddEdit)
-export class AddFarmEvent extends React.Component<AddEditFarmEventProps, {}> {
-  updateSequenceOrRegimen = (e: Partial<FarmEvent>) => {
+export class EditFarmEvent extends React.Component<AddEditFarmEventProps, {}> {
+  updateSequenceOrRegimen = (e: Partial<FarmEvent>) => (fe: TaggedFarmEvent) => {
     let { executable_id, executable_type } = e;
-    this.setState({ executable_id, executable_type });
+    this.props.dispatch(edit(fe, { executable_id, executable_type }));
   }
+
+  /** Determine if its safe to use a string as a `keyof FarmEvent`.
+   * Good for sanitizing user input and such.
+   */
+  isKeyofFarmEvent = hasKey<FarmEvent>([
+    "start_time",
+    "end_time",
+    "repeat",
+    "time_unit",
+    "next_time"
+  ]);
 
   updateForm = (e: React.SyntheticEvent<HTMLInputElement>) => {
-    switch (e.currentTarget.name) {
-      case "start_time":
-      case "end_time":
-      case "repeat":
-      case "time_unit":
-      case "next_time":
-        let { name, value } = e.currentTarget;
-        return this.setState({ [name]: value });
-      default:
-        throw new Error("Tried to match field name but couldn't.");
-    }
+    let { name, value } = e.currentTarget;
+    if (this.isKeyofFarmEvent(name)) {
+      return this.setState({ [name]: value });
+    } else {
+      throw new Error("Got bad key: " + name);
+    };
   }
 
-  // Waiting until we figure out the fb_select deal before borrowing interfaces
-  updateRepeatSelect = (e: { label: string, value: string, name: string }) => {
-    this.setState({ time_unit: e.value });
-  }
-
-  updateTime = (e: React.SyntheticEvent<HTMLInputElement>) => {
-    let { handleTime } = this.props;
-    switch (e.currentTarget.name) {
-      case "start_time":
-        // let newStart = handleTime(e, (this.state.start_time || "").toString());
-        // this.setState({ start_time: newStart });
-        break;
-      case "end_time":
-        // let newEnd = handleTime(e, (this.state.end_time || "").toString());
-        // this.setState({ end_time: newEnd });
-        break;
+  updateRepeatSelect = (e: DropDownItem) =>
+    (fe: TaggedFarmEvent) => {
+      this.props.dispatch(edit(fe, { time_unit: e.value }));
     }
-  }
 
-  handleDate = (e: React.SyntheticEvent<HTMLInputElement>) => {
-    switch (e.currentTarget.name) {
-      case "start_date":
-        let newStartDate = moment(e.currentTarget.value || "").toISOString();
-        this.setState({ start_time: newStartDate });
-        break;
-      case "end_date":
-        let newEndDate = moment(e.currentTarget.value || "").toISOString();
-        this.setState({ end_time: newEndDate });
-        break;
-      default:
-        throw new Error("Expected a name attribute from date field.");
+  updateTime = (e: React.SyntheticEvent<HTMLInputElement>) =>
+    (fe: TaggedFarmEvent) => {
+      let { handleTime } = this.props;
+      switch (e.currentTarget.name) {
+        case "start_time":
+          let newStart = handleTime(e, (fe.body.start_time || "").toString());
+          this.props.dispatch(edit(fe, { start_time: newStart }));
+          break;
+        case "end_time":
+          let newEnd = handleTime(e, (fe.body.end_time || "").toString());
+          this.props.dispatch(edit(fe, { end_time: newEnd }));
+          break;
+      }
     }
+
+  handleDate = (e: React.SyntheticEvent<HTMLInputElement>) =>
+    (fe: TaggedFarmEvent) => {
+      switch (e.currentTarget.name) {
+        case "start_date":
+          let newStartDate = moment(e.currentTarget.value || "").toISOString();
+          this.props.dispatch(edit(fe, { start_time: newStartDate }));
+          break;
+        case "end_date":
+          let newEndDate = moment(e.currentTarget.value || "").toISOString();
+          this.props.dispatch(edit(fe, { end_time: newEndDate }));
+          break;
+        default:
+          throw new Error("Expected a name attribute from date field.");
+      }
+    }
+
+  initialValue = (fe: TaggedFarmEvent) => {
+    let iv = { label: "Loading...", value: "Loading..." };
+    if (fe.body.executable_id && fe.body.executable_type) {
+      switch (fe.body.executable_type) {
+        case "Sequence":
+          let seq = this.props.sequencesById[fe.body.executable_id];
+          if (seq && seq.body.id) {
+            iv.label = seq.body.name;
+            iv.value = JSON.stringify(seq.body.id);
+          }
+          break;
+        case "Regimen":
+          let reg = this.props.regimensById[fe.body.executable_id];
+          if (reg && reg.body.id) {
+            iv.label = reg.body.name;
+            iv.value = JSON.stringify(reg.body.id);
+          }
+          break;
+      }
+    }
+    return iv;
   }
 
   render() {
     let fe = this.props.getFarmEvent(history.getCurrentLocation().pathname);
-    let { formatDate, formatTime, selectOptions } = this.props;
+    let { formatDate, formatTime, repeatOptions, dispatch } = this.props;
+    let { time_unit } = fe.body;
+    let currentTimeUnit = _.findWhere(repeatOptions, { value: time_unit });
+
     return <div className={`panel-container magenta-panel
-            add-farm-event-panel`}>
+      add-farm-event-panel`}>
       <div className="panel-header magenta-panel">
         <p className="panel-title">
-          <BackArrow /> {t("Add Farm Event")}
+          <BackArrow /> {t("Edit Farm Event")}
         </p>
       </div>
       <div className="panel-content">
         <label>{t("Sequence or Regimen")}</label>
         <FBSelect
-          list={selectOptions}
-          onChange={this.updateSequenceOrRegimen} />
+          list={this.props.selectOptions}
+          onChange={(e) => this.updateSequenceOrRegimen(e)(fe)}
+          initialValue={this.initialValue(fe)} />
         <label>{t("Starts")}</label>
         <Row>
           <Col xs={6}>
@@ -92,7 +132,7 @@ export class AddFarmEvent extends React.Component<AddEditFarmEventProps, {}> {
               name="start_date"
               value={formatDate((fe.body.start_time ||
                 new Date()).toString())}
-              onCommit={this.handleDate} />
+              onCommit={(e) => this.handleDate(e)(fe)} />
           </Col>
           <Col xs={6}>
             <BlurableInput type="time"
@@ -100,7 +140,7 @@ export class AddFarmEvent extends React.Component<AddEditFarmEventProps, {}> {
               name="start_time"
               value={formatTime((fe.body.start_time ||
                 new Date()).toString())}
-              onCommit={this.updateTime} />
+              onCommit={(e) => this.updateTime(e)(fe)} />
           </Col>
         </Row>
         <label>{t("Repeats Every")}</label>
@@ -117,7 +157,8 @@ export class AddFarmEvent extends React.Component<AddEditFarmEventProps, {}> {
           <Col xs={8}>
             <FBSelect
               list={this.props.repeatOptions}
-              onChange={this.updateRepeatSelect} />
+              onChange={(e) => this.updateRepeatSelect(e)(fe)}
+              initialValue={currentTimeUnit} />
           </Col>
         </Row>
         <label>{t("Until")}</label>
@@ -129,7 +170,7 @@ export class AddFarmEvent extends React.Component<AddEditFarmEventProps, {}> {
               name="end_date"
               value={formatDate((fe.body.end_time ||
                 new Date()).toString())}
-              onCommit={this.handleDate} />
+              onCommit={(e) => this.handleDate(e)(fe)} />
           </Col>
           <Col xs={6}>
             <BlurableInput
@@ -138,12 +179,28 @@ export class AddFarmEvent extends React.Component<AddEditFarmEventProps, {}> {
               className="add-event-end-time"
               value={formatTime((fe.body.end_time ||
                 new Date()).toString())}
-              onCommit={this.updateTime} />
+              onCommit={(e) => this.updateTime(e)(fe)} />
           </Col>
         </Row>
         <button className="magenta button-like"
-          onClick={() => { }}>
+          onClick={() => {
+            dispatch(save(fe.uuid))
+              .then(() => {
+                history.push("/app/designer/farm_events");
+                success("Saved farm event.", "Saved");
+              });
+          }}>
           {t("Save")}
+        </button>
+        <button className="red button-like"
+          onClick={() => {
+            dispatch(destroy(fe.uuid))
+              .then(() => {
+                history.push("/app/designer/farm_events");
+                success("Deleted farm event.", "Deleted");
+              });
+          }}>
+          {t("Delete")}
         </button>
       </div>
     </div>;
