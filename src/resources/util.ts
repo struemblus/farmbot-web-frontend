c6fa569545498cd6553e79abed4b2087c20a5881
MrChristofferson <chrisvan0848@gmail.com>
2017-04-04 07:44:16 -0700

    new generateUuid fn (was having problems with sorting before)

--- src/resources/util.ts
+++ src/resources/util.ts
@@ -1,7 +1,8 @@
 import { ResourceName } from "./tagged_resources";
-import { uuid } from "farmbot/dist";
+// import { uuid } from "farmbot/dist";
 import { joinKindAndId } from "./reducer";
 
+var count = 0;
 export function generateUuid(id: number | undefined, kind: ResourceName) {
-  return `${joinKindAndId(kind, id)}.${uuid()}`
+  return `${joinKindAndId(kind, id)}.${count++}`
 }
