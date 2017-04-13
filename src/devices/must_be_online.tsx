24bcdc13d3dde0f4910b3de79ab412c477b3e2ee
Rick Carlino <rick.carlino@gmail.com>
2017-04-13 11:24:38 -0500

    Unit tests + manual testing on staging for device panel

--- src/devices/must_be_online.tsx
+++ src/devices/must_be_online.tsx
@@ -1,15 +1,16 @@
 import * as React from "react";
 import { SyncStatus } from "farmbot/dist";
 
-interface Props {
+/** Properties for the <MustBeOnline/> element. */
+export interface MBOProps {
   status: SyncStatus | undefined;
   lockOpen?: boolean;
   fallback?: string | undefined;
   children?: React.ReactNode;
 }
 
-export function MustBeOnline({ children, lockOpen, fallback }: Props) {
-  if (lockOpen || (status && (status === "unknown"))) {
+export function MustBeOnline({ children, lockOpen, fallback, status }: MBOProps) {
+  if (lockOpen || (status && (status !== "unknown"))) {
     return <div> {children} </div>;
   } else {
     return <div> {fallback || ""} </div>;
