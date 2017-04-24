84e001418b69eadfde167715e96aa080e8dc4472
MrChristofferson <chrisvan0848@gmail.com>
2017-04-24 10:09:21 -0700

    css and translations

--- src/farm_designer/plants/plant_inventory_item.tsx
+++ src/farm_designer/plants/plant_inventory_item.tsx
@@ -5,32 +5,35 @@ import { t } from "i18next";
 import { TaggedPlant } from "../../resources/tagged_resources";
 import { push } from "../../history";
 
-type DIVEvent = React.SyntheticEvent<HTMLDivElement>
 type IMGEvent = React.SyntheticEvent<HTMLImageElement>
 
 // The inidividual plants that show up in the farm designer sub nav.
 export function PlantInventoryItem(props: TaggedPlant) {
 
-  // CSS to make apparent the associated mapped plant about to be clicked.
-  let mouseEnter = (plantId: string) => (e: DIVEvent) => {
-    console.log(plantId, e);
+  // For brevity's sake.
+  let plant = props.body;
 
-    let { classList } = e.currentTarget;
-    classList.add("eligible");
+  // Needed for React keys and navigating to correct plant_info page.
+  let plantId = (plant.id || "ERR_NO_PLANT_ID").toString();
+
+  // CSS to make apparent the associated mapped plant about to be clicked.
+  let mouseEnter = (id: string) => {
+    console.log(plantId);
+
+    // classList.add("eligible");
   }
 
   // Just removes the previous styling.
-  let mouseLeave = (plantId: string) => (e: DIVEvent) => {
-    let { classList } = e.currentTarget;
-    classList.remove("eligible");
+  let mouseLeave = (id: string) => {
+    // classList.remove("eligible");
   }
 
   // Handler for navigation and CSS.
-  let handleClick = (plantId: string) => (e: DIVEvent) => {
-    let { classList } = e.currentTarget;
+  let handleClick = (id: string) => {
+    // let { classList } = e.currentTarget;
     push("/app/designer/plants/" + plantId);
-    classList.remove("eligible");
-    classList.add("chosen");
+    // classList.remove("eligible");
+    // classList.add("chosen");
   }
 
   // See `cachedIcon` for more details on this.
@@ -39,11 +42,8 @@ export function PlantInventoryItem(props: TaggedPlant) {
     let img = e.currentTarget;
     // DEFAULT_ICON will be fallback.
     OFS && cachedIcon(OFS)
-      .then(i => img.setAttribute("src", i));
+      .then(iconString => img.setAttribute("src", iconString));
   }
-
-  // For brevity's sake.
-  let plant = props.body;
 
   // Name given from OpenFarm's API.
   let label = plant.name || "Unknown plant";
@@ -53,13 +53,10 @@ export function PlantInventoryItem(props: TaggedPlant) {
   let currentDay = moment();
   let daysOld = currentDay.diff(moment(plantedAt), "days") + 1;
 
-  // Needed for React keys and navigating to correct plant_info page.
-  let plantId = (plant.id || "ERR_NO_PLANT_ID").toString();
-
   return <div className="plant-search-item" key={plantId}
-    onMouseEnter={mouseEnter(plantId)}
-    onMouseLeave={mouseLeave(plantId)}
-    onClick={handleClick(plantId)}>
+    onMouseEnter={() => mouseEnter(plantId)}
+    onMouseLeave={() => mouseLeave(plantId)}
+    onClick={() => handleClick(plantId)}>
     <img className="plant-search-item-image"
       src={DEFAULT_ICON} onLoad={maybeGetCachedIcon} />
     <span className=";plant-search-item-name">{label}</span>
--- src/front_page/front_page.tsx
+++ src/front_page/front_page.tsx
@@ -7,6 +7,7 @@ import { prettyPrintApiErrors } from "../util";
 import { API } from "../api";
 import { Session } from "../session";
 import { FrontPageState } from "./interfaces";
+import { t } from "i18next";
 
 export class FrontPage extends React.Component<{}, Partial<FrontPageState>> {
   constructor() {
@@ -85,7 +86,7 @@ export class FrontPage extends React.Component<{}, Partial<FrontPageState>> {
     };
     axios.post<AuthState>(API.current.usersPath, form).then(resp => {
       let m = "Almost done! Check your email for the verification link.";
-      success(i18next.t(m));
+      success(t(m));
     }).catch(error => {
       log(prettyPrintApiErrors(error));
     });
@@ -118,14 +119,14 @@ export class FrontPage extends React.Component<{}, Partial<FrontPageState>> {
       const PRV_URL = process.env.PRIV_URL;
       return <div>
         <div>
-          <label>{i18next.t("I agree to the terms of use")}</label>
+          <label>{t("I agree to the terms of use")}</label>
           <input type="checkbox"
             onChange={this.set("agreeToTerms").bind(this)}
             value={this.state.agreeToTerms ? "false" : "true"} />
         </div>
         <ul>
-          <li><a href={PRV_URL}>Privacy Policy</a></li>
-          <li><a href={TOS_URL}>Terms of Use </a></li>
+          <li><a href={PRV_URL}>{t("Privacy Policy")}</a></li>
+          <li><a href={TOS_URL}>{t("Terms of Use")}</a></li>
         </ul>
       </div>;
     }
@@ -135,7 +136,8 @@ export class FrontPage extends React.Component<{}, Partial<FrontPageState>> {
     let buttonStylesUniqueToOnlyThisPage = {
       marginTop: "1.5rem",
       padding: ".5rem 1.6rem",
-      fontSize: "1.2rem"
+      fontSize: "1.2rem",
+      borderBottom: "none"
     };
 
     let { showServerOpts, forgotPassword } = this.state;
@@ -144,16 +146,17 @@ export class FrontPage extends React.Component<{}, Partial<FrontPageState>> {
     return (
       <div className="static-page">
         <h1>
-          Welcome to the FarmBot Web App
-                </h1>
+          {t("Welcome to the FarmBot Web App")}
+        </h1>
         <h2 className="fb-desktop-show">
-          Setup, customize, and control FarmBot from your computer
-                </h2>
+          {t("Setup, customize, and control FarmBot from your computer")}
+        </h2>
         <h2 className="fb-tablet-show">
-          Setup, customize, and control FarmBot from your tablet
-                </h2>
-        <h2 className="fb-mobile-show">Setup, customize, and control
-                FarmBot from your smartphone</h2>
+          {t("Setup, customize, and control FarmBot from your tablet")}
+        </h2>
+        <h2 className="fb-mobile-show">
+          {t("Setup, customize, and control FarmBot from your smartphone")}
+        </h2>
         <div className="image-login-wrapper">
           <div className="image-wrapper">
             <img className="fb-desktop-show"
@@ -168,7 +171,7 @@ export class FrontPage extends React.Component<{}, Partial<FrontPageState>> {
                   <div className="row">
                     <div className="col-sm-12">
                       <div className="widget-header">
-                        <h5>{i18next.t("Login")}</h5>
+                        <h5>{t("Login")}</h5>
                         <i className={`fa fa-${expandIcon}`}
                           onClick={toggleServerOpts}>
                         </i>
@@ -180,27 +183,27 @@ export class FrontPage extends React.Component<{}, Partial<FrontPageState>> {
                       <div className="col-sm-12">
                         <div className="widget-content">
                           <div className="input-group">
-                            <label> {i18next.t("Email")} </label>
+                            <label>{t("Email")}</label>
                             <input type="email"
                               onChange={this.set("email").bind(this)}>
                             </input>
-                            <label>{i18next.t("Password")}</label>
+                            <label>{t("Password")}</label>
                             <input type="password"
                               onChange={this.set("loginPassword").bind(this)}>
                             </input>
                             <a
                               className="forgot-password"
                               onClick={this.toggleForgotPassword.bind(this)}>
-                              Forgot password?
-                                                    </a>
+                              {t("Forgot password?")}
+                            </a>
                             {this.state.showServerOpts && (
                               <div>
-                                <label>{i18next.t("Server URL")}</label>
+                                <label>{t("Server URL")}</label>
                                 <input type="text"
                                   onChange={this.set("serverURL").bind(this)}
                                   value={this.state.serverURL}>
                                 </input>
-                                <label>{i18next.t("Server Port")}</label>
+                                <label>{t("Server Port")}</label>
                                 <input type="text"
                                   onChange={this.set("serverPort").bind(this)}
                                   value={this.state.serverPort}>
@@ -212,7 +215,7 @@ export class FrontPage extends React.Component<{}, Partial<FrontPageState>> {
                             <div className="col-xs-12">
                               <button className="green"
                                 style={buttonStylesUniqueToOnlyThisPage}>
-                                {i18next.t("Login")}
+                                {t("Login")}
                               </button>
                             </div>
                           </div>
@@ -229,11 +232,11 @@ export class FrontPage extends React.Component<{}, Partial<FrontPageState>> {
                   <div className="row">
                     <div className="col-sm-12">
                       <div className="widget-header">
-                        <h5>{i18next.t("Forgot Password")}</h5>
+                        <h5>{t("Forgot Password")}</h5>
                         <button
                           className="gray"
                           onClick={this.toggleForgotPassword.bind(this)}>
-                          {i18next.t("BACK")}
+                          {t("BACK")}
                         </button>
                       </div>
                     </div>
@@ -243,7 +246,7 @@ export class FrontPage extends React.Component<{}, Partial<FrontPageState>> {
                       <div className="col-sm-12">
                         <div className="widget-content">
                           <div className="input-group">
-                            <label>{i18next.t("Enter Email")}</label>
+                            <label>{t("Enter Email")}</label>
                             <input type="email"
                               onChange={this.set("email").bind(this)}>
                             </input>
@@ -252,7 +255,7 @@ export class FrontPage extends React.Component<{}, Partial<FrontPageState>> {
                             <div className="col-xs-12">
                               <button className="green"
                                 style={buttonStylesUniqueToOnlyThisPage}>
-                                {i18next.t("Send Password reset")}
+                                {t("Send Password reset")}
                               </button>
                             </div>
                           </div>
@@ -268,7 +271,7 @@ export class FrontPage extends React.Component<{}, Partial<FrontPageState>> {
                 <div className="row">
                   <div className="col-sm-12">
                     <div className="widget-header">
-                      <h5> {i18next.t("Create An Account")} </h5>
+                      <h5> {t("Create An Account")} </h5>
                     </div>
                   </div>
                 </div>
@@ -277,15 +280,15 @@ export class FrontPage extends React.Component<{}, Partial<FrontPageState>> {
                     <form onSubmit={this.submitRegistration.bind(this)} >
                       <div className="widget-content">
                         <div className="input-group">
-                          <label>{i18next.t("Email")} </label>
+                          <label>{t("Email")} </label>
                           <input type="email" onChange={this.set("regEmail").bind(this)} ></input>
-                          <label>Name</label>
+                          <label>{t("Name")}</label>
                           <input type="text" onChange={this.set("regName").bind(this)}></input>
-                          <label>Password</label>
+                          <label>{t("Password")}</label>
                           <input type="password"
                             onChange={this.set("regPassword").bind(this)}>
                           </input>
-                          <label>{i18next.t("Verify Password")}</label>
+                          <label>{t("Verify Password")}</label>
                           <input type="password"
                             onChange={
                               this.set("regConfirmation").bind(this)}>
@@ -296,7 +299,7 @@ export class FrontPage extends React.Component<{}, Partial<FrontPageState>> {
                           <div className="col-xs-12">
                             <button className="green"
                               style={buttonStylesUniqueToOnlyThisPage}>
-                              {i18next.t("Create Account")}
+                              {t("Create Account")}
                             </button>
                           </div>
                         </div>
