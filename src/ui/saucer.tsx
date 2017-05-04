75a137cbc15dd9a59b10d58cfbe953d8b129a49d
MrChristofferson <chrisvan0848@gmail.com>
2017-05-04 11:49:26 -0700

    fix regimen and sequence lists

--- src/ui/saucer.tsx
+++ src/ui/saucer.tsx
@@ -7,7 +7,7 @@ export interface SaucerProps {
 
 /** A colored UI disc/circle. */
 export function Saucer({ color, active }: SaucerProps) {
-  let className = `saucer saucer-${color}`;
+  let className = `saucer ${color}`;
   if (active) { className += " active"; }
   return <div className={className} />;
 }
