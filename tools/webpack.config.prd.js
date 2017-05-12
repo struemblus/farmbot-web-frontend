ebcc204eb8172bb00bbe36eac2a7454e58926bf3
Rick Carlino <rick.carlino@gmail.com>
2017-05-12 11:04:06 -0500

    Temporarily disable chunk hashing to see if it fixes staging

--- tools/webpack.config.prd.js
+++ tools/webpack.config.prd.js
@@ -11,47 +11,50 @@ var UglifyJsPlugin = require("webpack-uglify-js-plugin");
 var generateConfig = require("./webpack.config.base");
 var FarmBotRenderer = require("./farmBotRenderer");
 
-c = function() {
+c = function () {
 
-    var conf = generateConfig();
+  var conf = generateConfig();
 
-    conf.module.rules.push({
-        test: [/\.scss$/, /\.css$/],
-        loader: ExtractTextPlugin.extract("css-loader!sass-loader")
-    });
+  conf.module.rules.push({
+    test: [/\.scss$/, /\.css$/],
+    loader: ExtractTextPlugin.extract("css-loader!sass-loader")
+  });
 
-    conf.output.filename = "dist/[name].[chunkhash].js";
+  conf.output.filename = "dist/[name].[chunkhash].js";
 
-    // PLUGINS:
-    [
-        new webpack.DefinePlugin({
-            "process.env.NODE_ENV": JSON.stringify("production"),
-            "process.env.REVISION": JSON.stringify(
-                exec("git log --pretty=format:'%h%n%ad%n%f' -1").toString())
-        }),
-        new ExtractTextPlugin({
-            filename: "dist/styles.[chunkhash].css",
-            disable: false,
-            allChunks: true
-        }),
-        new OptimizeCssAssetsPlugin({
-            assetNameRegExp: /\.css$/g,
-            cssProcessor: require("cssnano"),
-            cssProcessorOptions: { discardComments: { removeAll: true } },
-            canPrint: true
-        }),
-        new UglifyJsPlugin({
-            cacheFolder: path.resolve(__dirname, "../public/dist/cached_uglify/"),
-            debug: true,
-            minimize: true,
-            sourceMap: true,
-            screw_ie8: true,
-            output: { comments: false },
-            compressor: { warnings: false }
-        })
-    ].forEach(function(x) { conf.plugins.push(x) });
+  // PLUGINS:
+  [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+      "process.env.REVISION": JSON.stringify(
+        exec("git log --pretty=format:'%h%n%ad%n%f' -1").toString())
+    }),
+    new ExtractTextPlugin({
+      // Temporary hotfix for some issues on staging.
+      // - RC 12 MAY 17
+      filename: "dist/styles.css",
+      // filename: "dist/styles.[chunkhash].css",
+      disable: false,
+      allChunks: true
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require("cssnano"),
+      cssProcessorOptions: { discardComments: { removeAll: true } },
+      canPrint: true
+    }),
+    new UglifyJsPlugin({
+      cacheFolder: path.resolve(__dirname, "../public/dist/cached_uglify/"),
+      debug: true,
+      minimize: true,
+      sourceMap: true,
+      screw_ie8: true,
+      output: { comments: false },
+      compressor: { warnings: false }
+    })
+  ].forEach(function (x) { conf.plugins.push(x) });
 
-    return conf;
+  return conf;
 
 }
 
