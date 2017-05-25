9c2cdbafdf0d2523f46eb1a7bd14212864d01263
MrChristofferson <chrisvan0848@gmail.com>
2017-05-25 17:07:24 -0500

    rm stub

--- src/farm_designer/map/layers/selected_plant_layer.tsx
+++ src/farm_designer/map/layers/selected_plant_layer.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 
 export function SelectedPlantLayer(props: any) {
-  return <circle cx="100" cy="100" r="100" />
+  return <circle cx="100" cy="100" r="100" fill="transparent" />
 }
