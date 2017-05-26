eb8d475578df3e218543c651d9fcd3616851a8d9
Rick Carlino <rick.carlino@gmail.com>
2017-05-26 08:00:37 -0500

    FBJS V. 3.9.8 version bump

--- src/sequences/edit_step.ts
+++ src/sequences/edit_step.ts
@@ -1,6 +1,6 @@
 import { Sequence } from "./interfaces";
 
-export let editStep(dispatch: Function, sequence: Sequence, ) {
+export let editStep = (dispatch: Function, sequence: Sequence) => {
   debugger;
   console.warn("Dry up all that replace()ing of steps here")
 };
