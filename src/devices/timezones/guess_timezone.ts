c948167d57507bbfc0f8cb58c01154c991b7080c
Rick Carlino <rick.carlino@gmail.com>
2017-06-14 18:55:18 -0500

    Bring back the alert(), throttle stuff

--- src/devices/timezones/guess_timezone.ts
+++ src/devices/timezones/guess_timezone.ts
@@ -1,27 +1,35 @@
 
 import { warning } from "../../ui/index";
-
+/** Remove this in October 2017 - RC */
+let ONLY_ONCE = {
+  need_to_talk: true
+}
 export function inferTimezone(current: string | undefined): string {
   if (current) {
     return current;
   }
 
   if (Intl && Intl.DateTimeFormat) {
-    warning("This account did not have a timezone set. " +
-      "Farmbot requires a timezone to operate. " +
-      "We have updated your timezone settings based on your browser. " +
-      "Please verify these settings in the device settings panel. " +
-      "Device sync is recommended.")
+    if (ONLY_ONCE.need_to_talk) {
+      warning("This account did not have a timezone set. " +
+        "Farmbot requires a timezone to operate. " +
+        "We have updated your timezone settings based on your browser. " +
+        "Please verify these settings in the device settings panel. " +
+        "Device sync is recommended.");
+      ONLY_ONCE.need_to_talk = false;
+    }
     // WARNING SIDE EFFECTS!!!
     return Intl
       .DateTimeFormat()
       .resolvedOptions()
       .timeZone;
   }
-
-  warning("Warning: Farmbot could not guess your timezone. " +
-    "Please select your timezone " +
-    "from the dropdown. " +
-    "Device sync is recommended.");
+  if (ONLY_ONCE.need_to_talk) {
+    warning("Warning: Farmbot could not guess your timezone. " +
+      "Please select your timezone " +
+      "from the dropdown. " +
+      "Device sync is recommended.");
+    ONLY_ONCE.need_to_talk = false;
+  };
   return "UTC";
 }
