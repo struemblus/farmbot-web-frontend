7df5a4b371eabca440feb3c2160d4e219588c956
MrChristofferson <chrisvan0848@gmail.com>
2017-06-16 16:21:38 -0500

    farm event styles

--- src/farm_designer/farm_events/tz_warning.tsx
+++ src/farm_designer/farm_events/tz_warning.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
+import { t } from "i18next";
 import { timezoneMismatch } from "../../devices/timezones/guess_timezone";
 
-const WARNING = <span>Note: Times displayed according to local browser time,
-  which is currently different from your device timezone setting (on the
-  Device page).</span>;
+const WARNING = `Note: Times displayed according to local browser time, which
+is currently different from your device timezone setting (on the Device page).`;
 
 interface TzWarningProps {
   deviceTimezone: string | undefined;
 }
 
 export function TzWarning({ deviceTimezone }: TzWarningProps) {
-  return <div>
-    {timezoneMismatch(deviceTimezone) ? WARNING : ""}
+  return <div className="note">
+    {timezoneMismatch(deviceTimezone) ? t(WARNING) : ""}
   </div>;
 }
