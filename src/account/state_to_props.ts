637c4961d3da24439e029464051ba29a5f775e96
Rick Carlino <rick.carlino@gmail.com>
2017-06-16 10:46:49 -0500

    Done removing dependency on JWT for user info.

--- src/account/state_to_props.ts
+++ src/account/state_to_props.ts
@@ -3,7 +3,7 @@ import { deleteUser } from "./actions";
 import { Props } from "./interfaces";
 import { getUserAccountSettings } from "../resources/selectors";
 import { User } from "../auth/interfaces";
-import { edit } from "../api/crud";
+import { edit, save } from "../api/crud";
 
 export function mapStateToProps(props: Everything): Props {
   let user = getUserAccountSettings(props.resources.index);
@@ -12,6 +12,7 @@ export function mapStateToProps(props: Everything): Props {
     user,
     saveUser(dispatch: Function, update: Partial<User>) {
       dispatch(edit(user, update));
+      dispatch(save(user.uuid))
     },
     enactDeletion(dispatch: Function, password: string | undefined) {
       dispatch(deleteUser({ password: password || "NEVER SET" }));
