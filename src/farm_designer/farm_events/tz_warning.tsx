56825761cfe8827e2e9d41930f9e0ca3c29aa644
Rick Carlino <rick.carlino@gmail.com>
2017-06-19 07:17:48 -0500

    Verbiage change

--- src/farm_designer/farm_events/tz_warning.tsx
+++ src/farm_designer/farm_events/tz_warning.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
 import { timezoneMismatch } from "../../devices/timezones/guess_timezone";
 
-const WARNING = <span>Note: Times displayed according to local browser time,
-  which is currently different from your device timezone setting (on the
-  Device page).</span>;
+const WARNING = <span>Note: The selected timezone for your FarmBot is different
+  than your local browser time..</span>;
 
 interface TzWarningProps {
   deviceTimezone: string | undefined;
